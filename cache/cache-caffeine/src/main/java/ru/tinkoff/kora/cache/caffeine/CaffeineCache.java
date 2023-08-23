21beca66853ed96e8803c69e5578203e56b42cdc
Anton Kurako <goodforgod.dev@gmail.com>
2023-08-23 19:51:21 +0000

    CaffeineCache#getAll added

--- cache/cache-caffeine/src/main/java/ru/tinkoff/kora/cache/caffeine/CaffeineCache.java
+++ cache/cache-caffeine/src/main/java/ru/tinkoff/kora/cache/caffeine/CaffeineCache.java
@@ -1,7 +1,16 @@
 package ru.tinkoff.kora.cache.caffeine;
 
+import reactor.core.publisher.Mono;
 import ru.tinkoff.kora.cache.Cache;
+
+import javax.annotation.Nonnull;
+import java.util.Map;
 
 public interface CaffeineCache<K, V> extends Cache<K, V> {
 
+    /**
+     * @return all values and keys
+     */
+    @Nonnull
+    Map<K, V> getAll();
 }
