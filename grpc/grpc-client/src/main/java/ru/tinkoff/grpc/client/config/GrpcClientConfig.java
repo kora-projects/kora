25fc501110ed421e3d0231b0d4e88d1f963ef6cb
Anton Kurako <goodforgod.dev@gmail.com>
2023-10-20 13:17:56 +0300

    GrpcClientConfig suffix fixed

--- grpc/grpc-client/src/main/java/ru/tinkoff/grpc/client/config/GrpcClientConfig.java
+++ grpc/grpc-client/src/main/java/ru/tinkoff/grpc/client/config/GrpcClientConfig.java
@@ -18,6 +18,11 @@ public interface GrpcClientConfig {
     TelemetryConfig telemetry();
 
     static GrpcClientConfig defaultConfig(Config config, ru.tinkoff.kora.config.common.extractor.ConfigValueExtractor<GrpcClientConfig> extractor, String serviceName) {
-        return Objects.requireNonNull(extractor.extract(config.get("grpcClient." + serviceName)));
+        var packageEnding = serviceName.lastIndexOf('.');
+        var serviceSimpleName = (packageEnding == -1)
+                ? serviceName
+                : serviceName.substring(packageEnding + 1);
+
+        return Objects.requireNonNull(extractor.extract(config.get("grpcClient." + serviceSimpleName)));
     }
 }
