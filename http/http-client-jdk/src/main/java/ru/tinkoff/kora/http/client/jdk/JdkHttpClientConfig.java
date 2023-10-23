c8850751d79624678c4464ab9be0745742266b4d
Anton Duyun <anton.duyun@gmail.com>
2023-10-24 00:00:55 +0300

    Jdk http client should use http 1.1 as default

--- http/http-client-jdk/src/main/java/ru/tinkoff/kora/http/client/jdk/JdkHttpClientConfig.java
+++ http/http-client-jdk/src/main/java/ru/tinkoff/kora/http/client/jdk/JdkHttpClientConfig.java
@@ -2,9 +2,15 @@ package ru.tinkoff.kora.http.client.jdk;
 
 import ru.tinkoff.kora.config.common.annotation.ConfigValueExtractor;
 
+import java.net.http.HttpClient;
+
 @ConfigValueExtractor
 public interface JdkHttpClientConfig {
     default int threads() {
         return Runtime.getRuntime().availableProcessors() * 2;
+    }
+
+    default HttpClient.Version httpVersion() {
+        return HttpClient.Version.HTTP_1_1;
     }
 }
