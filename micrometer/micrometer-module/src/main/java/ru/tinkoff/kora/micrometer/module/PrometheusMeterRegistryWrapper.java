df8859553aa78fa547e94093e73c5396c0817bd9
Anton Duyun <anton.duyun@gmail.com>
2023-12-08 19:08:26 +0300

    Kora up metric with version

--- micrometer/micrometer-module/src/main/java/ru/tinkoff/kora/micrometer/module/PrometheusMeterRegistryWrapper.java
+++ micrometer/micrometer-module/src/main/java/ru/tinkoff/kora/micrometer/module/PrometheusMeterRegistryWrapper.java
@@ -1,5 +1,6 @@
 package ru.tinkoff.kora.micrometer.module;
 
+import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.Metrics;
 import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics;
 import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics;
@@ -13,12 +14,17 @@ import io.micrometer.prometheus.PrometheusMeterRegistry;
 import ru.tinkoff.kora.application.graph.Lifecycle;
 import ru.tinkoff.kora.application.graph.Wrapped;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 public final class PrometheusMeterRegistryWrapper implements Lifecycle, Wrapped<PrometheusMeterRegistry> {
+    private static final String KORA_VERSION = readVersion();
+
     private final List<PrometheusMeterRegistryInitializer> initializers;
     private volatile PrometheusMeterRegistry registry;
     private volatile JvmGcMetrics gcMetrics;
+    private volatile Gauge koraVersionMetric;
 
     public PrometheusMeterRegistryWrapper(List<PrometheusMeterRegistryInitializer> initializers) {
         this.initializers = initializers;
@@ -40,21 +46,32 @@ public final class PrometheusMeterRegistryWrapper implements Lifecycle, Wrapped<
         new UptimeMetrics().bindTo(meterRegistry);
         this.registry = meterRegistry;
         Metrics.addRegistry(this.registry);
+        this.koraVersionMetric = Gauge.builder("kora.up", 1, i -> i)
+            .tag("version", KORA_VERSION)
+            .register(this.registry);
     }
+
 
     @Override
     public void release() {
         var r = this.registry;
         var gcMetrics = this.gcMetrics;
+        var koraVersion = this.koraVersionMetric;
         try {
             if (gcMetrics != null) {
                 gcMetrics.close();
             }
         } finally {
-            if (r != null) {
-                this.registry = null;
-                Metrics.removeRegistry(r);
-                r.close();
+            try {
+                if (koraVersion != null) {
+                    koraVersion.close();
+                }
+            } finally {
+                if (r != null) {
+                    this.registry = null;
+                    Metrics.removeRegistry(r);
+                    r.close();
+                }
             }
         }
     }
@@ -63,4 +80,17 @@ public final class PrometheusMeterRegistryWrapper implements Lifecycle, Wrapped<
     public PrometheusMeterRegistry value() {
         return this.registry;
     }
+
+    private static String readVersion() {
+        try (var is = Thread.currentThread().getContextClassLoader().getResourceAsStream("META-INF/kora/version/common")) {
+            if (is == null) {
+                return "UNKNOWN";
+            }
+            var bytes = is.readAllBytes();
+            return new String(bytes, StandardCharsets.US_ASCII);
+        } catch (IOException e) {
+            return "UNKNOWN";
+        }
+    }
+
 }
