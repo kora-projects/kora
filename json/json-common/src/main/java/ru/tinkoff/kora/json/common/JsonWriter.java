81d5954683bf45d11ed9d2fa66cecbd37488abc6
Anton Kurako <goodforgod.dev@gmail.com>
2023-12-19 14:41:09 +0000

    JsonWriter toString contract added

--- json/json-common/src/main/java/ru/tinkoff/kora/json/common/JsonWriter.java
+++ json/json-common/src/main/java/ru/tinkoff/kora/json/common/JsonWriter.java
@@ -2,11 +2,13 @@ package ru.tinkoff.kora.json.common;
 
 import com.fasterxml.jackson.core.JsonEncoding;
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.io.SegmentedStringWriter;
 import com.fasterxml.jackson.core.util.ByteArrayBuilder;
-
 import jakarta.annotation.Nullable;
+
 import java.io.IOException;
 import java.io.UncheckedIOException;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Class that defines public API for writing JSON content.
@@ -35,6 +37,24 @@ public interface JsonWriter<T> {
     default byte[] toByteArrayUnchecked(@Nullable T value) throws UncheckedIOException {
         try {
             return toByteArray(value);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    default String toString(@Nullable T value) throws IOException {
+        try (var sw = new SegmentedStringWriter(JsonCommonModule.JSON_FACTORY._getBufferRecycler());
+             var gen = JsonCommonModule.JSON_FACTORY.createGenerator(sw)) {
+            gen.enable(JsonGenerator.Feature.WRITE_BIGDECIMAL_AS_PLAIN);
+            this.write(gen, value);
+            gen.flush();
+            return sw.getAndClear();
+        }
+    }
+
+    default String toStringUnchecked(@Nullable T value) throws UncheckedIOException {
+        try {
+            return toString(value);
         } catch (IOException e) {
             throw new UncheckedIOException(e);
         }
