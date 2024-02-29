7300ba29c0303096c5d49881513f2c96aaf95840
Anton Kurako <goodforgod.dev@gmail.com>
2024-03-01 01:48:35 +0300

    [dependencies] gRPC import fixed

--- grpc/grpc-client/src/main/java/ru/tinkoff/grpc/client/GrpcClientChannelFactory.java
+++ grpc/grpc-client/src/main/java/ru/tinkoff/grpc/client/GrpcClientChannelFactory.java
@@ -2,17 +2,19 @@ package ru.tinkoff.grpc.client;
 
 import io.grpc.ChannelCredentials;
 import io.grpc.ManagedChannelBuilder;
-import io.grpc.internal.GrpcUtil;
 
 import java.net.SocketAddress;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public interface GrpcClientChannelFactory {
+
     default ManagedChannelBuilder<?> forAddress(String host, int port) {
-        return forTarget(GrpcUtil.authorityFromHostAndPort(host, port));
+        return forTarget(authorityFromHostAndPort(host, port));
     }
 
     default ManagedChannelBuilder<?> forAddress(String host, int port, ChannelCredentials creds) {
-        return forTarget(GrpcUtil.authorityFromHostAndPort(host, port), creds);
+        return forTarget(authorityFromHostAndPort(host, port), creds);
     }
 
     ManagedChannelBuilder<?> forAddress(SocketAddress serverAddress);
@@ -22,4 +24,12 @@ public interface GrpcClientChannelFactory {
     ManagedChannelBuilder<?> forTarget(String target);
 
     ManagedChannelBuilder<?> forTarget(String target, ChannelCredentials creds);
+
+    private static String authorityFromHostAndPort(String host, int port) {
+        try {
+            return new URI(null, null, host, port, null, null, null).getAuthority();
+        } catch (URISyntaxException ex) {
+            throw new IllegalArgumentException("Invalid host or port: " + host + " " + port, ex);
+        }
+    }
 }
