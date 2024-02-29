65af67a606a1750aa4cc26b1256e9c43c007df66
Anton Kurako <goodforgod.dev@gmail.com>
2024-02-29 23:48:18 +0300

    [graalvm] GraalNativeImageHints.java cleanup

--- config/config-hocon/src/main/java/ru/tinkoff/kora/config/hocon/GraalNativeImageHints.java
+++
@@ -1,9 +0,0 @@
-package ru.tinkoff.kora.config.hocon;
-
-import io.goodforgod.graalvm.hint.annotation.ResourceHint;
-
-@ResourceHint(include = {"application.conf", "reference.conf"})
-final class GraalNativeImageHints {
-
-    private GraalNativeImageHints() { }
-}
