bf46c225bfcac93e4c8dde491a8b09199f06bf64
Anton Duyun <anton.duyun@gmail.com>
2024-03-26 14:52:20 +0300

    grpc server metrics

--- grpc/grpc-server/src/main/java/ru/tinkoff/kora/grpc/server/telemetry/GrpcServerMetrics.java
+++ grpc/grpc-server/src/main/java/ru/tinkoff/kora/grpc/server/telemetry/GrpcServerMetrics.java
@@ -3,7 +3,7 @@ package ru.tinkoff.kora.grpc.server.telemetry;
 import io.grpc.Status;
 
 public interface GrpcServerMetrics {
-    void onClose(Status status, Throwable exception, long processingTime);
+    void onClose(Status status, Throwable exception, long processingTimeNano);
 
     void onSend(Object message);
 
--- micrometer/micrometer-module/src/main/java/ru/tinkoff/kora/micrometer/module/grpc/server/MicrometerGrpcServerMetricsFactory.java
+++ micrometer/micrometer-module/src/main/java/ru/tinkoff/kora/micrometer/module/grpc/server/MicrometerGrpcServerMetricsFactory.java
@@ -2,14 +2,17 @@ package ru.tinkoff.kora.micrometer.module.grpc.server;
 
 import io.grpc.Metadata;
 import io.grpc.ServerCall;
+import io.grpc.Status;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.DistributionSummary;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.opentelemetry.semconv.SemanticAttributes;
 import ru.tinkoff.kora.grpc.server.telemetry.GrpcServerMetrics;
 import ru.tinkoff.kora.grpc.server.telemetry.GrpcServerMetricsFactory;
 import ru.tinkoff.kora.telemetry.common.TelemetryConfig;
 
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
 
 public final class MicrometerGrpcServerMetricsFactory implements GrpcServerMetricsFactory {
     private final ConcurrentHashMap<MetricsKey, GrpcServerMetrics> metrics = new ConcurrentHashMap<>();
@@ -27,25 +30,32 @@ public final class MicrometerGrpcServerMetricsFactory implements GrpcServerMetri
     }
 
     private GrpcServerMetrics buildMetrics(TelemetryConfig.MetricsConfig config, MetricsKey metricsKey) {
-        var duration = DistributionSummary.builder("rpc.server.duration")
+        var duration = (Function<Status, DistributionSummary>) status -> DistributionSummary.builder("rpc.server.duration")
             .serviceLevelObjectives(config.slo())
-            .baseUnit("milliseconds")
-            .tag("rpc.system", "grpc")
-            .tag("rpc.service", metricsKey.serviceName)
-            .tag("rpc.method", metricsKey.methodName)
+            .baseUnit(switch (config.spec()) {
+                case V120 -> "milliseconds";
+                case V123 -> "s";
+            })
+            .tag(SemanticAttributes.RPC_SYSTEM.getKey(), SemanticAttributes.RpcSystemValues.GRPC)
+            .tag(SemanticAttributes.RPC_SERVICE.getKey(), metricsKey.serviceName)
+            .tag(SemanticAttributes.RPC_METHOD.getKey(), metricsKey.methodName)
+            .tag(SemanticAttributes.RPC_GRPC_STATUS_CODE.getKey(), Integer.toString(status.getCode().value()))
             .register(this.meterRegistry);
         var requestsPerRpc = Counter.builder("rpc.server.requests_per_rpc")
             .baseUnit("messages")
-            .tag("rpc.system", "grpc")
-            .tag("rpc.service", metricsKey.serviceName)
-            .tag("rpc.method", metricsKey.methodName)
+            .tag(SemanticAttributes.RPC_SYSTEM.getKey(), SemanticAttributes.RpcSystemValues.GRPC)
+            .tag(SemanticAttributes.RPC_SERVICE.getKey(), metricsKey.serviceName)
+            .tag(SemanticAttributes.RPC_METHOD.getKey(), metricsKey.methodName)
             .register(this.meterRegistry);
         var responsesPerRpc = Counter.builder("rpc.server.responses_per_rpc")
             .baseUnit("messages")
-            .tag("rpc.system", "grpc")
-            .tag("rpc.service", metricsKey.serviceName)
-            .tag("rpc.method", metricsKey.methodName)
+            .tag(SemanticAttributes.RPC_SYSTEM.getKey(), SemanticAttributes.RpcSystemValues.GRPC)
+            .tag(SemanticAttributes.RPC_SERVICE.getKey(), metricsKey.serviceName)
+            .tag(SemanticAttributes.RPC_METHOD.getKey(), metricsKey.methodName)
             .register(this.meterRegistry);
-        return new MicrometerGrpcServerMetrics(duration, requestsPerRpc, responsesPerRpc);
+        return switch (config.spec()) {
+            case V120 -> new Opentelemetry120GrpcServerMetrics(duration, requestsPerRpc, responsesPerRpc);
+            case V123 -> new Opentelemetry123GrpcServerMetrics(duration, requestsPerRpc, responsesPerRpc);
+        };
     }
 }
