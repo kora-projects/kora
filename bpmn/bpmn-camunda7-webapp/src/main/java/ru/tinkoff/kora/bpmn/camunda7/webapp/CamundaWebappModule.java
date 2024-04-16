1d4bb6acb9d24fa2744b3639b24db9ec5d53f7d4
Anton Kurako <goodforgod.dev@gmail.com>
2024-04-16 12:00:24 +0300

    Webapp removed

--- bpmn/bpmn-camunda7-webapp/src/main/java/ru/tinkoff/kora/bpmn/camunda7/webapp/CamundaWebappModule.java
+++
@@ -1,5 +0,0 @@
-package ru.tinkoff.kora.bpmn.camunda7.webapp;
-
-public interface CamundaWebappModule extends CamundaWebappAdminModule, CamundaWebappCockpitModule, CamundaWebappTasklistModule, CamundaWebappWelcomeModule {
-
-}
