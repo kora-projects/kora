45d0b9431c616f2ccfb536b6bf2a9495f9b3e214
Anton Kurako <goodforgod.dev@gmail.com>
2024-05-02 17:48:59 +0300

    UndertowHttpServer dependency removed UndertowCamundaHttpServer moved to camunda-rest module

--- camunda/camunda-rest-undertow/src/main/java/ru/tinkoff/kora/camunda/rest/undertow/CamundaRestUndertowModule.java
+++ camunda/camunda-rest-undertow/src/main/java/ru/tinkoff/kora/camunda/rest/undertow/CamundaRestUndertowModule.java
@@ -3,14 +3,13 @@ package ru.tinkoff.kora.camunda.rest.undertow;
 import io.undertow.server.HttpHandler;
 import jakarta.ws.rs.core.Application;
 import ru.tinkoff.kora.application.graph.All;
+import ru.tinkoff.kora.application.graph.Lifecycle;
 import ru.tinkoff.kora.application.graph.ValueOf;
 import ru.tinkoff.kora.camunda.rest.CamundaRest;
 import ru.tinkoff.kora.camunda.rest.CamundaRestConfig;
 import ru.tinkoff.kora.camunda.rest.CamundaRestModule;
 import ru.tinkoff.kora.common.Tag;
 import ru.tinkoff.kora.common.annotation.Root;
-import ru.tinkoff.kora.http.server.common.HttpServer;
-import ru.tinkoff.kora.http.server.common.HttpServerConfig;
 
 public interface CamundaRestUndertowModule extends CamundaRestModule {
 
@@ -22,9 +21,8 @@ public interface CamundaRestUndertowModule extends CamundaRestModule {
 
     @Tag(CamundaRest.class)
     @Root
-    default HttpServer camundaRestHttpServer(@Tag(CamundaRest.class) ValueOf<HttpHandler> camundaHttpHandler,
-                                             ValueOf<CamundaRestConfig> camundaRestConfig,
-                                             ValueOf<HttpServerConfig> httpServerConfig) {
-        return new UndertowCamundaRestHttpServer(camundaHttpHandler, camundaRestConfig, httpServerConfig);
+    default Lifecycle camundaRestHttpServer(@Tag(CamundaRest.class) ValueOf<HttpHandler> camundaHttpHandler,
+                                            ValueOf<CamundaRestConfig> camundaRestConfig) {
+        return new UndertowCamundaHttpServer(camundaRestConfig, camundaHttpHandler);
     }
 }
