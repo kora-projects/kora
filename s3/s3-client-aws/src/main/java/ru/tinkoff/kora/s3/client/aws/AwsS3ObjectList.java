76f4cab6a7ce518321a763cc5a035e4f64b39377
Anton Kurako <goodforgod.dev@gmail.com>
2024-05-23 12:48:22 +0300

    Some AWS impls are public just in case

--- s3/s3-client-aws/src/main/java/ru/tinkoff/kora/s3/client/aws/AwsS3ObjectList.java
+++ s3/s3-client-aws/src/main/java/ru/tinkoff/kora/s3/client/aws/AwsS3ObjectList.java
@@ -4,6 +4,7 @@ import ru.tinkoff.kora.s3.client.model.S3Object;
 import ru.tinkoff.kora.s3.client.model.S3ObjectList;
 import ru.tinkoff.kora.s3.client.model.S3ObjectMeta;
 import ru.tinkoff.kora.s3.client.model.S3ObjectMetaList;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
 
 import java.util.List;
 
@@ -12,9 +13,9 @@ final class AwsS3ObjectList implements S3ObjectList {
     private final S3ObjectMetaList metaList;
     private final List<S3Object> objects;
 
-    public AwsS3ObjectList(S3ObjectMetaList metaList, List<S3Object> objects) {
+    public AwsS3ObjectList(ListObjectsV2Response response, List<S3Object> objects) {
         this.objects = objects;
-        this.metaList = metaList;
+        this.metaList = new AwsS3ObjectMetaList(response);
     }
 
     @Override
