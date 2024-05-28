a712eb7f7a877f7135339c72fa7e736ca18e52d2
Anton Kurako <goodforgod.dev@gmail.com>
2024-05-28 17:41:50 +0300

    Javadoc added

--- s3/s3-client-common/src/main/java/ru/tinkoff/kora/s3/client/annotation/S3.java
+++ s3/s3-client-common/src/main/java/ru/tinkoff/kora/s3/client/annotation/S3.java
@@ -1,6 +1,7 @@
 package ru.tinkoff.kora.s3.client.annotation;
 
 import org.jetbrains.annotations.Range;
+import ru.tinkoff.kora.s3.client.S3ClientConfig;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -11,10 +12,22 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.CLASS)
 public @interface S3 {
 
+    /**
+     * Annotation for S3 Client interface contract
+     */
     @Target({ElementType.TYPE})
     @Retention(RetentionPolicy.CLASS)
     @interface Client {
 
+        /**
+         * @return path for {@link S3ClientConfig} configuration
+         * <pre>{@code
+         *     @S3.Client("my-client")
+         *     interface SomeClient {
+         *
+         *     }
+         * }</pre>
+         */
         String value() default "";
     }
 
@@ -22,6 +35,20 @@ public @interface S3 {
     @Retention(RetentionPolicy.CLASS)
     @interface Get {
 
+        /**
+         * @return Specifies key template or key constant:
+         * <pre>{@code
+         *     @S3.Client("my-client")
+         *     interface SomeClient {
+         *
+         *         @S3.Get("prefix-{key}")
+         *         S3Object get1(String key);
+         *
+         *         @S3.Get("const-key")
+         *         S3Object get2();
+         *     }
+         * }</pre>
+         */
         String value() default "";
     }
 
@@ -29,6 +56,20 @@ public @interface S3 {
     @Retention(RetentionPolicy.CLASS)
     @interface List {
 
+        /**
+         * @return Specifies key template or key constant:
+         * <pre>{@code
+         *     @S3.Client("my-client")
+         *     interface SomeClient {
+         *
+         *         @S3.List("pre-{prefix}")
+         *         S3ObjectList list1(String prefix);
+         *
+         *         @S3.List("const-key")
+         *         S3ObjectList list2();
+         *     }
+         * }</pre>
+         */
         String value() default "";
 
         @Range(from = 1, to = 1000)
@@ -39,10 +80,30 @@ public @interface S3 {
     @Retention(RetentionPolicy.CLASS)
     @interface Put {
 
+        /**
+         * @return Specifies key template or key constant:
+         * <pre>{@code
+         *     @S3.Client("my-client")
+         *     interface SomeClient {
+         *
+         *         @S3.Put("prefix-{key}")
+         *         void put1(String key, S3Body body);
+         *
+         *         @S3.Put("const-key")
+         *         void put2(S3Body body);
+         *     }
+         * }</pre>
+         */
         String value() default "";
 
+        /**
+         * @return Content-Type value like: image/jpeg
+         */
         String type() default "";
 
+        /**
+         * @return Content-Encoding value like: gzip
+         */
         String encoding() default "";
     }
 
@@ -50,6 +111,20 @@ public @interface S3 {
     @Retention(RetentionPolicy.CLASS)
     @interface Delete {
 
+        /**
+         * @return Specifies key template or key constant:
+         * <pre>{@code
+         *     @S3.Client("my-client")
+         *     interface SomeClient {
+         *
+         *         @S3.Put("prefix-{key}")
+         *         void delete1(String key);
+         *
+         *         @S3.Put("const-key")
+         *         void delete2();
+         *     }
+         * }</pre>
+         */
         String value() default "";
     }
 }
