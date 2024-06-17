0cc8a603890a2f63255dd8587e59f1a66f93f6b6
ᴀɴᴛᴏɴ <GoodforGod@users.noreply.github.com>
2024-06-17 17:52:32 +0700

    Undertow Xnio Executor Tagged (#56)
    Undertow Xnio Executor with tag

--- http/http-server-undertow/src/main/java/ru/tinkoff/kora/http/server/undertow/UndertowHttpServerModule.java
+++ http/http-server-undertow/src/main/java/ru/tinkoff/kora/http/server/undertow/UndertowHttpServerModule.java
@@ -1,10 +1,12 @@
 package ru.tinkoff.kora.http.server.undertow;
 
+import io.undertow.Undertow;
 import io.undertow.connector.ByteBufferPool;
 import jakarta.annotation.Nullable;
 import org.xnio.XnioWorker;
 import ru.tinkoff.kora.application.graph.ValueOf;
 import ru.tinkoff.kora.common.DefaultComponent;
+import ru.tinkoff.kora.common.Tag;
 import ru.tinkoff.kora.common.annotation.Root;
 import ru.tinkoff.kora.http.server.common.HttpServerConfig;
 import ru.tinkoff.kora.http.server.common.handler.BlockingRequestExecutor;
@@ -19,12 +21,15 @@ public interface UndertowHttpServerModule extends UndertowModule {
     }
 
     @Root
-    default UndertowHttpServer undertowHttpServer(ValueOf<HttpServerConfig> config, ValueOf<UndertowPublicApiHandler> handler, XnioWorker worker, ByteBufferPool byteBufferPool) {
+    default UndertowHttpServer undertowHttpServer(ValueOf<HttpServerConfig> config,
+                                                  ValueOf<UndertowPublicApiHandler> handler,
+                                                  @Tag(Undertow.class) XnioWorker worker,
+                                                  ByteBufferPool byteBufferPool) {
         return new UndertowHttpServer(config, handler, worker, byteBufferPool);
     }
 
     @DefaultComponent
-    default BlockingRequestExecutor undertowBlockingRequestExecutor(XnioWorker xnioWorker) {
+    default BlockingRequestExecutor undertowBlockingRequestExecutor(@Tag(Undertow.class) XnioWorker xnioWorker) {
         return new BlockingRequestExecutor.Default(xnioWorker);
     }
 
