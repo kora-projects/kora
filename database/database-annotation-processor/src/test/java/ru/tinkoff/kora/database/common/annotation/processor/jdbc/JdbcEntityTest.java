441c4ec21289cfd3889d0df7bc6a4b9b698e2ab1
Anton Duyun <anton.duyun@gmail.com>
2024-07-04 20:19:07 +0300

    Nullability detection should work with any `Nullable` annotation

--- database/database-annotation-processor/src/test/java/ru/tinkoff/kora/database/common/annotation/processor/jdbc/JdbcEntityTest.java
+++ database/database-annotation-processor/src/test/java/ru/tinkoff/kora/database/common/annotation/processor/jdbc/JdbcEntityTest.java
@@ -49,4 +49,33 @@ public class JdbcEntityTest extends AbstractJdbcEntityTest {
         var rowWithNull = rowMapper.apply(rs);
         assertThat(rowWithNull).isEqualTo(newObject("TestRecord", null, 20));
     }
+
+    @Test
+    public void testFieldNullableOnCanonicalConstructor() throws SQLException {
+        var expectedType = ParameterizedTypeName.get(ClassName.get(JdbcRowMapper.class), className("TestRecord"));
+
+        var graph = compile(expectedType, List.of(),
+            """
+                public record TestRecord(Integer f1, Integer f2) {
+                    public TestRecord(@Nullable Integer f1, Integer f2) {
+                        this.f1 = java.util.Objects.requireNonNullElse(f1, 10);
+                        this.f2 = java.util.Objects.requireNonNull(f2);
+                    }
+                    public TestRecord() {
+                        this(10, 20);
+                    }
+                }
+                """
+        );
+        assertThat(draw.getNodes()).hasSize(2);
+
+        var rowMapper = (JdbcRowMapper<?>) graph.get(draw.getNodes().get(0));
+        var rs = mockResultSet(
+            of("f1", (Integer) null),
+            of("f2", 20)
+        );
+
+        var rowWithNull = rowMapper.apply(rs);
+        assertThat(rowWithNull).isEqualTo(newObject("TestRecord", 10, 20));
+    }
 }
