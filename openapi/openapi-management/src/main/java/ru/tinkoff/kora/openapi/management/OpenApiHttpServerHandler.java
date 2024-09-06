9999d2163bf7be84df091beec012631777839aef
Anton Kurako <goodforgod.dev@gmail.com>
2024-09-06 12:19:23 +0300

    OpenAPI Management support for multiple OpenAPI files

--- openapi/openapi-management/src/main/java/ru/tinkoff/kora/openapi/management/OpenApiHttpServerHandler.java
+++ openapi/openapi-management/src/main/java/ru/tinkoff/kora/openapi/management/OpenApiHttpServerHandler.java
@@ -5,41 +5,65 @@ import ru.tinkoff.kora.http.common.body.HttpBody;
 import ru.tinkoff.kora.http.server.common.HttpServerRequest;
 import ru.tinkoff.kora.http.server.common.HttpServerResponse;
 import ru.tinkoff.kora.http.server.common.HttpServerResponseException;
-import ru.tinkoff.kora.http.server.common.handler.BlockingRequestExecutor;
 import ru.tinkoff.kora.http.server.common.handler.HttpServerRequestHandler;
 
 import java.io.IOException;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicReference;
 
 final class OpenApiHttpServerHandler implements HttpServerRequestHandler.HandlerFunction {
 
-    private final String contentType;
-    private final byte[] openapi;
+    record OpenapiFile(String fileName, String filePath, String contentType, AtomicReference<byte[]> content) {}
+
+    private final Map<String, OpenapiFile> openapiFiles = new ConcurrentHashMap<>();
 
     OpenApiHttpServerHandler(OpenApiManagementConfig config) {
-        this.contentType = config.file().endsWith(".json")
+        for (String filePath : config.file()) {
+            var contentType = filePath.endsWith(".json")
                 ? "text/json; charset=utf-8"
                 : "text/x-yaml; charset=utf-8";
-
-        this.openapi = loadOpenapi(config);
+            final String fileName = ResourceUtils.getFileName(filePath);
+            this.openapiFiles.put(fileName, new OpenapiFile(fileName, filePath, contentType, new AtomicReference<>()));
+        }
     }
 
     @Override
     public CompletionStage<HttpServerResponse> apply(Context context, HttpServerRequest request) throws Exception {
-        return CompletableFuture.completedFuture(HttpServerResponse.of(200, HttpBody.of(contentType, openapi)));
+        final String fileName = (openapiFiles.size() == 1) ? openapiFiles.keySet().iterator().next() : request.pathParams().get("file");
+        if (fileName == null || fileName.isEmpty()) {
+            return CompletableFuture.completedFuture(HttpServerResponse.of(400, HttpBody.plaintext("OpenAPI file not specified")));
+        }
+
+        final OpenapiFile openapiFile = openapiFiles.get(fileName);
+        if (openapiFile == null) {
+            return CompletableFuture.completedFuture(HttpServerResponse.of(404, HttpBody.plaintext("OpenAPI file not registered: " + fileName)));
+        }
+
+        byte[] bytes = openapiFile.content().get();
+        if (bytes != null) {
+            return CompletableFuture.completedFuture(HttpServerResponse.of(200, HttpBody.of(openapiFile.contentType(), bytes)));
+        }
+
+        return CompletableFuture.supplyAsync(() -> {
+            byte[] fileContent = loadOpenapi(openapiFile.filePath());
+            openapiFile.content().set(fileContent);
+            return HttpServerResponse.of(200, HttpBody.of(openapiFile.contentType(), fileContent));
+        });
     }
 
-    private static byte[] loadOpenapi(OpenApiManagementConfig config) {
+    private static byte[] loadOpenapi(String filePath) {
         try {
-            var openapiAsStream = ResourceUtils.getFileAsStream(config.file());
+            var openapiAsStream = ResourceUtils.getFileAsStream(filePath);
             if (openapiAsStream == null) {
-                throw HttpServerResponseException.of(404, "Can't read direct OpenAPI file: " + config.file());
+                throw HttpServerResponseException.of(404, "OpenAPI file not found while reading: " + filePath);
             }
 
             return openapiAsStream.readAllBytes();
         } catch (IOException e) {
-            throw HttpServerResponseException.of(500, "Can't read direct OpenAPI file: " + config.file());
+            throw HttpServerResponseException.of(500, "Can't read OpenAPI file: " + filePath);
         }
     }
 }
--- openapi/openapi-management/src/main/java/ru/tinkoff/kora/openapi/management/OpenApiManagementModule.java
+++ openapi/openapi-management/src/main/java/ru/tinkoff/kora/openapi/management/OpenApiManagementModule.java
@@ -2,7 +2,6 @@ package ru.tinkoff.kora.openapi.management;
 
 import ru.tinkoff.kora.config.common.Config;
 import ru.tinkoff.kora.config.common.extractor.ConfigValueExtractor;
-import ru.tinkoff.kora.http.common.HttpMethod;
 import ru.tinkoff.kora.http.server.common.HttpServerResponse;
 import ru.tinkoff.kora.http.server.common.handler.HttpServerRequestHandler;
 import ru.tinkoff.kora.http.server.common.handler.HttpServerRequestHandlerImpl;
@@ -16,38 +15,39 @@ public interface OpenApiManagementModule {
     }
 
     default HttpServerRequestHandler openApiManagementController(OpenApiManagementConfig config) {
+        final String path = (config.file().size() == 1) ? config.endpoint() : config.endpoint() + "/{file}";
         if (!config.enabled()) {
-            return HttpServerRequestHandlerImpl.of(HttpMethod.GET, config.endpoint(),
-                    (context, request) -> CompletableFuture.completedFuture(HttpServerResponse.of(404)));
+            return HttpServerRequestHandlerImpl.get(path,
+                (context, request) -> CompletableFuture.completedFuture(HttpServerResponse.of(404)));
         }
 
         var handler = new OpenApiHttpServerHandler(config);
-        return HttpServerRequestHandlerImpl.of(HttpMethod.GET, config.endpoint(), handler);
+        return HttpServerRequestHandlerImpl.get(path, handler);
     }
 
     default HttpServerRequestHandler swaggerUIManagementController(OpenApiManagementConfig config) {
         if (config.swaggerui() == null) {
-            return HttpServerRequestHandlerImpl.of(HttpMethod.GET, "/swagger-ui",
-                    (context, request) -> CompletableFuture.completedFuture(HttpServerResponse.of(404)));
+            return HttpServerRequestHandlerImpl.get("/swagger-ui",
+                (context, request) -> CompletableFuture.completedFuture(HttpServerResponse.of(404)));
         } else if (!config.enabled() || !config.swaggerui().enabled()) {
-            return HttpServerRequestHandlerImpl.of(HttpMethod.GET, config.swaggerui().endpoint(),
-                    (context, request) -> CompletableFuture.completedFuture(HttpServerResponse.of(404)));
+            return HttpServerRequestHandlerImpl.get(config.swaggerui().endpoint(),
+                (context, request) -> CompletableFuture.completedFuture(HttpServerResponse.of(404)));
         }
 
         var handler = new SwaggerUIHttpServerHandler(config);
-        return HttpServerRequestHandlerImpl.of(HttpMethod.GET, config.swaggerui().endpoint(), handler);
+        return HttpServerRequestHandlerImpl.get(config.swaggerui().endpoint(), handler);
     }
 
     default HttpServerRequestHandler rapidocManagementController(OpenApiManagementConfig config) {
         if (config.rapidoc() == null) {
-            return HttpServerRequestHandlerImpl.of(HttpMethod.GET, "/rapidoc",
-                    (context, request) -> CompletableFuture.completedFuture(HttpServerResponse.of(404)));
+            return HttpServerRequestHandlerImpl.get("/rapidoc",
+                (context, request) -> CompletableFuture.completedFuture(HttpServerResponse.of(404)));
         } else if (!config.enabled() || !config.rapidoc().enabled()) {
-            return HttpServerRequestHandlerImpl.of(HttpMethod.GET, config.rapidoc().endpoint(),
-                    (context, request) -> CompletableFuture.completedFuture(HttpServerResponse.of(404)));
+            return HttpServerRequestHandlerImpl.get(config.rapidoc().endpoint(),
+                (context, request) -> CompletableFuture.completedFuture(HttpServerResponse.of(404)));
         }
 
         var handler = new RapidocHttpServerHandler(config);
-        return HttpServerRequestHandlerImpl.of(HttpMethod.GET, config.rapidoc().endpoint(), handler);
+        return HttpServerRequestHandlerImpl.get(config.rapidoc().endpoint(), handler);
     }
 }
