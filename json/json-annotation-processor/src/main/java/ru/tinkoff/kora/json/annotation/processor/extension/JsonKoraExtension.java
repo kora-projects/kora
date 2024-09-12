4924e833b4294fa004849c58078710f2375c1eff
ᴀɴᴛᴏɴ <GoodforGod@users.noreply.github.com>
2024-09-12 12:56:53 +0300

    Kotlin JSON extension constructor fallback (#118)
    * Kotlin JSON Writer/Reader find constructor fallback strategy
    
    * README.md updated
    
    ---------
    
    Co-authored-by: Anton Duyun <anton.duyun@gmail.com>

--- json/json-annotation-processor/src/main/java/ru/tinkoff/kora/json/annotation/processor/extension/JsonKoraExtension.java
+++ json/json-annotation-processor/src/main/java/ru/tinkoff/kora/json/annotation/processor/extension/JsonKoraExtension.java
@@ -176,13 +176,12 @@ public class JsonKoraExtension implements KoraExtension {
         return ExtensionResult.fromExecutable(constructor);
     }
 
-
     private ExecutableElement findDefaultConstructor(TypeElement resultElement) {
         return resultElement.getEnclosedElements()
             .stream()
             .filter(e -> e.getKind() == ElementKind.CONSTRUCTOR)
             .map(ExecutableElement.class::cast)
             .findFirst()
-            .orElseThrow();
+            .orElseThrow(() -> new ProcessingErrorException("No primary constructor found for " + resultElement, resultElement));
     }
 }
--- json/json-symbol-processor/src/main/kotlin/ru/tinkoff/kora/json/ksp/extension/JsonKoraExtension.kt
+++ json/json-symbol-processor/src/main/kotlin/ru/tinkoff/kora/json/ksp/extension/JsonKoraExtension.kt
@@ -140,7 +140,13 @@ class JsonKoraExtension(
     }
 
     private fun findDefaultConstructor(resultElement: KSClassDeclaration): KSFunctionDeclaration {
-        return resultElement.primaryConstructor ?: throw NoSuchElementException("No value present")
+        return if (resultElement.primaryConstructor != null) {
+            resultElement.primaryConstructor!!
+        } else if (resultElement.getConstructors().count() == 1) {
+            resultElement.getConstructors().first()
+        } else {
+            throw ProcessingErrorException("No primary constructor found for: $resultElement", resultElement)
+        }
     }
 
     private fun isProcessableType(typeRef: KSType): Boolean {
