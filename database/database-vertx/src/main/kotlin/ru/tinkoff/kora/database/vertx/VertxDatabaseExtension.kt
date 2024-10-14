1dbdd7f359f1e372755f9b338120248137e9c802
Anton Kurako <goodforgod.dev@gmail.com>
2024-10-14 17:11:22 +0300

    Suspend kotlin contract full support for JDBC/R2DBC/Vertx

---
+++ database/database-vertx/src/main/kotlin/ru/tinkoff/kora/database/vertx/VertxDatabaseExtension.kt
@@ -0,0 +1,37 @@
+package ru.tinkoff.kora.database.vertx
+
+import io.vertx.sqlclient.SqlClient
+import io.vertx.sqlclient.SqlConnection
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.future.await
+import kotlinx.coroutines.future.future
+import ru.tinkoff.kora.common.Context
+import kotlin.coroutines.coroutineContext
+
+suspend inline fun <T> VertxConnectionFactory.withConnectionSuspend(noinline callback: suspend (SqlClient) -> T): T {
+    val ctx = coroutineContext
+    val future = withConnection {
+        val current = Context.current()
+        CoroutineScope(ctx).future {
+            current.inject()
+            val res = callback.invoke(it)
+            Context.clear()
+            res
+        }
+    }
+    return future.await()
+}
+
+suspend inline fun <T> VertxConnectionFactory.inTxSuspend(noinline callback: suspend (SqlConnection) -> T): T {
+    val ctx = coroutineContext
+    val future = inTx {
+        val current = Context.current()
+        CoroutineScope(ctx).future<T>(ctx) {
+            current.inject()
+            val res = callback.invoke(it)
+            Context.clear()
+            res
+        }
+    }
+    return future.await()
+}
