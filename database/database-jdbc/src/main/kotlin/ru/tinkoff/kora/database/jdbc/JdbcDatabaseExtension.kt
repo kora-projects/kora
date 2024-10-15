1813b159626b2e908141befe495cccffccb4a097
Anton Kurako <goodforgod.dev@gmail.com>
2024-10-15 11:15:30 +0300

    JdbcBlocking suspend call

--- database/database-jdbc/src/main/kotlin/ru/tinkoff/kora/database/jdbc/JdbcDatabaseExtension.kt
+++ database/database-jdbc/src/main/kotlin/ru/tinkoff/kora/database/jdbc/JdbcDatabaseExtension.kt
@@ -3,34 +3,45 @@ package ru.tinkoff.kora.database.jdbc
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.future.await
 import kotlinx.coroutines.future.future
+import kotlinx.coroutines.runBlocking
 import ru.tinkoff.kora.common.Context
 import java.sql.Connection
 import kotlin.coroutines.coroutineContext
 
 suspend inline fun <T> JdbcConnectionFactory.withConnectionSuspend(noinline callback: suspend (Connection) -> T): T {
     val ctx = coroutineContext
-    val future = withConnectionStage {
-        val current = Context.current()
-        CoroutineScope(ctx).future {
-            current.inject()
-            val res = callback.invoke(it)
-            Context.clear()
-            res
-        }
+    val current = Context.current()
+    val future = CoroutineScope(ctx).future {
+        val old = Context.current()
+
+        val fork = current.fork()
+        fork.inject()
+
+        val result = withConnection(JdbcHelper.SqlFunction1 {
+            runBlocking { callback.invoke(it) }
+        })
+
+        old.inject()
+        result
     }
     return future.await()
 }
 
 suspend inline fun <T> JdbcConnectionFactory.inTxSuspend(noinline callback: suspend (Connection) -> T): T {
     val ctx = coroutineContext
-    val future = inTxStage {
-        val current = Context.current()
-        CoroutineScope(ctx).future {
-            current.inject()
-            val res = callback.invoke(it)
-            Context.clear()
-            res
-        }
+    val current = Context.current()
+    val future = CoroutineScope(ctx).future {
+        val old = Context.current()
+
+        val fork = current.fork()
+        fork.inject()
+
+        val result = inTx(JdbcHelper.SqlFunction1 {
+            runBlocking { callback.invoke(it) }
+        })
+
+        old.inject()
+        result
     }
     return future.await()
 }
