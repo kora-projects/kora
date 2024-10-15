938dd073339bd01c90545a256cc0f472461f0fde
Anton Kurako <goodforgod.dev@gmail.com>
2024-10-15 11:41:02 +0300

    JdbcDatabaseExtension.kt custom context support

--- database/database-jdbc/src/main/kotlin/ru/tinkoff/kora/database/jdbc/JdbcDatabaseExtension.kt
+++ database/database-jdbc/src/main/kotlin/ru/tinkoff/kora/database/jdbc/JdbcDatabaseExtension.kt
@@ -6,10 +6,11 @@ import kotlinx.coroutines.future.future
 import kotlinx.coroutines.runBlocking
 import ru.tinkoff.kora.common.Context
 import java.sql.Connection
+import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.coroutineContext
 
-suspend inline fun <T> JdbcConnectionFactory.withConnectionSuspend(noinline callback: suspend (Connection) -> T): T {
-    val ctx = coroutineContext
+suspend inline fun <T> JdbcConnectionFactory.withConnectionSuspend(context: CoroutineContext? = null, noinline callback: suspend (Connection) -> T): T {
+    val ctx = context ?: coroutineContext
     val current = Context.current()
     val future = CoroutineScope(ctx).future {
         val old = Context.current()
@@ -29,8 +30,8 @@ suspend inline fun <T> JdbcConnectionFactory.withConnectionSuspend(noinline call
     return future.await()
 }
 
-suspend inline fun <T> JdbcConnectionFactory.inTxSuspend(noinline callback: suspend (Connection) -> T): T {
-    val ctx = coroutineContext
+suspend inline fun <T> JdbcConnectionFactory.inTxSuspend(context: CoroutineContext? = null, noinline callback: suspend (Connection) -> T): T {
+    val ctx = context ?: coroutineContext
     val current = Context.current()
     val future = CoroutineScope(ctx).future {
         val old = Context.current()
