900ab65d0332abf012586d1205803c3e31b5f01d
Anton Duyun <anton.duyun@gmail.com>
2024-10-17 16:37:44 +0300

    java impl

--- database/database-annotation-processor/src/test/java/ru/tinkoff/kora/database/common/annotation/processor/jdbc/JdbcEntityTest.java
+++ database/database-annotation-processor/src/test/java/ru/tinkoff/kora/database/common/annotation/processor/jdbc/JdbcEntityTest.java
@@ -3,9 +3,10 @@ package ru.tinkoff.kora.database.common.annotation.processor.jdbc;
 import com.squareup.javapoet.ClassName;
 import com.squareup.javapoet.ParameterizedTypeName;
 import org.junit.jupiter.api.Test;
+import ru.tinkoff.kora.database.annotation.processor.jdbc.JdbcEntityAnnotationProcessor;
+import ru.tinkoff.kora.database.jdbc.mapper.result.JdbcResultSetMapper;
 import ru.tinkoff.kora.database.jdbc.mapper.result.JdbcRowMapper;
 
-import java.sql.SQLException;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -13,69 +14,77 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class JdbcEntityTest extends AbstractJdbcEntityTest {
 
     @Test
-    public void testFieldRequired() throws SQLException {
+    public void testAnnotatedRecordRowMapper() {
         var expectedType = ParameterizedTypeName.get(ClassName.get(JdbcRowMapper.class), className("TestRecord"));
 
-        var graph = compile(expectedType, List.of(),
+        var graph = compile(expectedType, List.of(), List.of(new JdbcEntityAnnotationProcessor()),
+            "@ru.tinkoff.kora.database.jdbc.JdbcEntity public record TestRecord(Integer f1, Integer f2){}"
+        );
+        assertThat(draw.getNodes()).hasSize(2);
+
+        assertThat(graph.get(draw.getNodes().get(0))).isInstanceOf(JdbcRowMapper.class);
+    }
+
+    @Test
+    public void testAnnotatedRecordResultSetMapper() {
+        var expectedType = ParameterizedTypeName.get(ClassName.get(JdbcResultSetMapper.class), className("TestRecord"));
+
+        var graph = compile(expectedType, List.of(), List.of(new JdbcEntityAnnotationProcessor()),
+            "@ru.tinkoff.kora.database.jdbc.JdbcEntity public record TestRecord(Integer f1, Integer f2){}"
+        );
+        assertThat(draw.getNodes()).hasSize(2);
+
+        assertThat(graph.get(draw.getNodes().get(0))).isInstanceOf(JdbcResultSetMapper.class);
+    }
+
+    @Test
+    public void testAnnotatedRecordListResultSetMapper() {
+        var expectedType = ParameterizedTypeName.get(ClassName.get(JdbcResultSetMapper.class), ParameterizedTypeName.get(ClassName.get(List.class), className("TestRecord")));
+
+        var graph = compile(expectedType, List.of(), List.of(new JdbcEntityAnnotationProcessor()),
+            "@ru.tinkoff.kora.database.jdbc.JdbcEntity public record TestRecord(Integer f1, Integer f2){}"
+        );
+        assertThat(draw.getNodes()).hasSize(2);
+
+        assertThat(graph.get(draw.getNodes().get(0))).isInstanceOf(JdbcResultSetMapper.class);
+    }
+
+    @Test
+    public void testNonAnnotatedRecordRowMapper() {
+        var expectedType = ParameterizedTypeName.get(ClassName.get(JdbcRowMapper.class), className("TestRecord"));
+
+        var graph = compile(expectedType, List.of(), List.of(new JdbcEntityAnnotationProcessor()),
             "public record TestRecord(Integer f1, Integer f2){}"
         );
         assertThat(draw.getNodes()).hasSize(2);
+        assertThat(compileResult.warnings()).hasSize(3);
 
-        var rowMapper = (JdbcRowMapper<?>) graph.get(draw.getNodes().get(0));
-
-        var rs = mockResultSet(
-            of("f1", 10),
-            of("f2", 20)
-        );
-        var row = rowMapper.apply(rs);
-        assertThat(row).isEqualTo(newObject("TestRecord", 10, 20));
+        assertThat(graph.get(draw.getNodes().get(0))).isInstanceOf(JdbcRowMapper.class);
     }
 
     @Test
-    public void testFieldNullable() throws SQLException {
-        var expectedType = ParameterizedTypeName.get(ClassName.get(JdbcRowMapper.class), className("TestRecord"));
+    public void testNonAnnotatedRecordResultSetMapper() {
+        var expectedType = ParameterizedTypeName.get(ClassName.get(JdbcResultSetMapper.class), className("TestRecord"));
 
-        var graph = compile(expectedType, List.of(),
-            "public record TestRecord(@Nullable Integer f1, Integer f2){}"
+        var graph = compile(expectedType, List.of(), List.of(new JdbcEntityAnnotationProcessor()),
+            "public record TestRecord(Integer f1, Integer f2){}"
         );
-        assertThat(draw.getNodes()).hasSize(2);
+        assertThat(draw.getNodes()).hasSize(3);
+        assertThat(compileResult.warnings()).hasSize(3);
 
-        var rowMapper = (JdbcRowMapper<?>) graph.get(draw.getNodes().get(0));
-        var rs = mockResultSet(
-            of("f1", (Integer) null),
-            of("f2", 20)
-        );
-
-        var rowWithNull = rowMapper.apply(rs);
-        assertThat(rowWithNull).isEqualTo(newObject("TestRecord", null, 20));
+        assertThat(graph.get(draw.getNodes().get(1))).isInstanceOf(JdbcResultSetMapper.class);
     }
 
     @Test
-    public void testFieldNullableOnCanonicalConstructor() throws SQLException {
-        var expectedType = ParameterizedTypeName.get(ClassName.get(JdbcRowMapper.class), className("TestRecord"));
+    public void testNonAnnotatedRecorListResultSetMapper() {
+        var expectedType = ParameterizedTypeName.get(ClassName.get(JdbcResultSetMapper.class), ParameterizedTypeName.get(ClassName.get(List.class), className("TestRecord")));
 
-        var graph = compile(expectedType, List.of(),
-            """
-                public record TestRecord(Integer f1, Integer f2) {
-                    public TestRecord(@Nullable Integer f1, Integer f2) {
-                        this.f1 = java.util.Objects.requireNonNullElse(f1, 10);
-                        this.f2 = java.util.Objects.requireNonNull(f2);
-                    }
-                    public TestRecord() {
-                        this(10, 20);
-                    }
-                }
-                """
+        var graph = compile(expectedType, List.of(), List.of(new JdbcEntityAnnotationProcessor()),
+            "public record TestRecord(Integer f1, Integer f2){}"
         );
         assertThat(draw.getNodes()).hasSize(2);
+        assertThat(compileResult.warnings()).hasSize(3);
 
-        var rowMapper = (JdbcRowMapper<?>) graph.get(draw.getNodes().get(0));
-        var rs = mockResultSet(
-            of("f1", (Integer) null),
-            of("f2", 20)
-        );
-
-        var rowWithNull = rowMapper.apply(rs);
-        assertThat(rowWithNull).isEqualTo(newObject("TestRecord", 10, 20));
+        assertThat(graph.get(draw.getNodes().get(0))).isInstanceOf(JdbcResultSetMapper.class);
     }
 }
