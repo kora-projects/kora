8613040e1dd8b0fa677bc6e7f0711c154bceeb6c
Anton Kurako <goodforgod.dev@gmail.com>
2024-10-21 15:38:39 +0300

    JdbcDatabaseExtension#withConnectionSuspendContext

--- database/database-jdbc/src/main/kotlin/ru/tinkoff/kora/database/jdbc/JdbcDatabaseExtension.kt
+++ database/database-jdbc/src/main/kotlin/ru/tinkoff/kora/database/jdbc/JdbcDatabaseExtension.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.withContext
 import org.jetbrains.annotations.ApiStatus.Internal
+import ru.tinkoff.kora.common.Context
 import java.sql.Connection
 import java.sql.SQLException
 import java.util.concurrent.Executor
@@ -14,6 +15,10 @@ import kotlin.coroutines.CoroutineContext
 @Internal
 @PublishedApi
 internal object JdbcDatabaseExtension {
+
+    fun getConnectionKey(jdbcDatabase: JdbcDatabase): Context.Key<Connection>? = jdbcDatabase.connectionKey
+
+    fun setConnectionKey(jdbcDatabase: JdbcDatabase, context: Context, connection: Connection): Connection = context.set(jdbcDatabase.connectionKey, connection)
 
     fun getExecutor(jdbcDatabase: JdbcDatabase): Executor? = jdbcDatabase.executor
 }
@@ -56,8 +61,70 @@ suspend fun <T> JdbcConnectionFactory.withConnectionSuspend(dispatcher: Coroutin
     }
 }
 
+suspend fun <T> JdbcConnectionFactory.withConnectionSuspendContext(userDispatcher: CoroutineDispatcher? = null, callback: suspend (Connection) -> T): T {
+    if (this !is JdbcDatabase) {
+        throw UnsupportedOperationException("Only JdbcDatabase is supported")
+    }
+
+    val factory = this
+    val dispatcher = userDispatcher
+        ?: JdbcDatabaseExtension.getExecutor(factory)?.asCoroutineDispatcher()
+        ?: Dispatchers.IO
+
+    val koraCurrent = Context.current()
+    val koraCrtContext = Context.Kotlin.asCoroutineContext(koraCurrent)
+    return withContext(dispatcher + koraCrtContext) {
+        val currentConnection = koraCurrent[JdbcDatabaseExtension.getConnectionKey(factory)]
+        if (currentConnection != null) {
+            try {
+                callback.invoke(currentConnection)
+            } catch (e: SQLException) {
+                throw RuntimeSqlException(e)
+            }
+        }
+
+        try {
+            val koraFork = koraCurrent.fork()
+            koraFork.inject()
+            val newConnection = newConnection()
+            JdbcDatabaseExtension.setConnectionKey(factory, koraFork, newConnection)
+            newConnection.use {
+                withContext(dispatcher + koraCrtContext + CoroutineConnection(it)) {
+                    callback.invoke(it)
+                }
+            }
+        } catch (e: SQLException) {
+            throw RuntimeSqlException(e)
+        }
+    }
+}
+
 suspend fun <T> JdbcConnectionFactory.inTxSuspend(context: CoroutineDispatcher? = null, callback: suspend (Connection) -> T): T {
     return this.withConnectionSuspend(context) { connection ->
+        if (!connection.autoCommit) {
+            callback.invoke(connection)
+        }
+
+        connection.autoCommit = false
+        try {
+            val result: T = callback.invoke(connection)
+            connection.commit()
+            connection.autoCommit = true
+            result
+        } catch (e: Exception) {
+            try {
+                connection.rollback()
+                connection.autoCommit = true
+            } catch (sqlException: SQLException) {
+                e.addSuppressed(sqlException)
+            }
+            throw e
+        }
+    }
+}
+
+suspend fun <T> JdbcConnectionFactory.inTxSuspendContext(context: CoroutineDispatcher? = null, callback: suspend (Connection) -> T): T {
+    return this.withConnectionSuspendContext(context) { connection ->
         if (!connection.autoCommit) {
             callback.invoke(connection)
         }
