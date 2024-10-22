e661d6c27b116f73090955c1e2f7628ae3080c42
Anton Duyun <anton.duyun@gmail.com>
2024-10-22 13:16:31 +0300

    fix

--- database/database-annotation-processor/src/test/java/ru/tinkoff/kora/database/common/annotation/processor/jdbc/entity/JdbcEntityAnnotationProcessorTest.java
+++ database/database-annotation-processor/src/test/java/ru/tinkoff/kora/database/common/annotation/processor/jdbc/entity/JdbcEntityAnnotationProcessorTest.java
@@ -18,7 +18,7 @@ public class JdbcEntityAnnotationProcessorTest extends AbstractAnnotationProcess
     @Test
     public void testRecordMappersGenerated() {
         compile(List.of(new JdbcEntityAnnotationProcessor()), """
-            import ru.tinkoff.kora.database.jdbc.JdbcEntity;
+            import ru.tinkoff.kora.database.jdbc.EntityJdbc;
             
             @JdbcEntity
             public record TestRecord(int id){}
@@ -38,7 +38,7 @@ public class JdbcEntityAnnotationProcessorTest extends AbstractAnnotationProcess
     @Test
     public void testUnknownFieldTypeRequiresMapper() {
         compile(List.of(new JdbcEntityAnnotationProcessor()), """
-            import ru.tinkoff.kora.database.jdbc.JdbcEntity;
+            import ru.tinkoff.kora.database.jdbc.EntityJdbc;
             
             @JdbcEntity
             public record TestRecord(TestRecord id){}
@@ -62,7 +62,7 @@ public class JdbcEntityAnnotationProcessorTest extends AbstractAnnotationProcess
     @Test
     public void testJavaBeanMappersGenerated() {
         compile(List.of(new JdbcEntityAnnotationProcessor()), """
-            import ru.tinkoff.kora.database.jdbc.JdbcEntity;
+            import ru.tinkoff.kora.database.jdbc.EntityJdbc;
             
             @JdbcEntity
             public class TestClass {
