fcdfdbdbdcc063fbfc73aa3c13920b052f7c703d
Anton Kurako <goodforgod.dev@gmail.com>
2024-11-05 16:44:49 +0300

    Cleanup

--- micrometer/micrometer-module/src/main/java/ru/tinkoff/kora/micrometer/module/s3/client/Opentelemetry123S3KoraClientMetrics.java
+++ micrometer/micrometer-module/src/main/java/ru/tinkoff/kora/micrometer/module/s3/client/Opentelemetry123S3KoraClientMetrics.java
@@ -2,20 +2,17 @@ package ru.tinkoff.kora.micrometer.module.s3.client;
 
 import io.micrometer.core.instrument.DistributionSummary;
 import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.Tag;
 import io.opentelemetry.api.common.AttributeKey;
 import jakarta.annotation.Nullable;
 import ru.tinkoff.kora.s3.client.S3Exception;
 import ru.tinkoff.kora.s3.client.telemetry.S3KoraClientMetrics;
 import ru.tinkoff.kora.telemetry.common.TelemetryConfig;
 
-import java.util.ArrayList;
 import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 
 import static io.opentelemetry.api.common.AttributeKey.stringKey;
 import static io.opentelemetry.semconv.SemanticAttributes.AWS_S3_BUCKET;
-import static io.opentelemetry.semconv.SemanticAttributes.AWS_S3_KEY;
 
 public class Opentelemetry123S3KoraClientMetrics implements S3KoraClientMetrics {
 
