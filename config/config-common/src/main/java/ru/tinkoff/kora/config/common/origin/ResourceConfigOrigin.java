22868cc5e1af9e00d6033cd585c641385e0d5c3a
ᴀɴᴛᴏɴ <GoodforGod@users.noreply.github.com>
2024-12-17 18:01:44 +0300

    ResourceConfigOrigin#description fixed (#206)
    ResourceConfigOrigin#description added

--- config/config-common/src/main/java/ru/tinkoff/kora/config/common/origin/ResourceConfigOrigin.java
+++ config/config-common/src/main/java/ru/tinkoff/kora/config/common/origin/ResourceConfigOrigin.java
@@ -3,8 +3,9 @@ package ru.tinkoff.kora.config.common.origin;
 import java.net.URL;
 
 public record ResourceConfigOrigin(URL url) implements ConfigOrigin {
+
     @Override
     public String description() {
-        return null;
+        return "Resource " + url;
     }
 }
