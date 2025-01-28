dc65bbc9d19c1305e07881717cc9dc50eed2a95d
Anton Kurako <goodforgod.dev@gmail.com>
2025-01-28 13:11:12 +0300

    ViolationExceptionHttpServerResponseMapper context added

--- validation/validation-module/src/main/java/ru/tinkoff/kora/validation/module/http/server/ValidationHttpServerInterceptor.java
+++ validation/validation-module/src/main/java/ru/tinkoff/kora/validation/module/http/server/ValidationHttpServerInterceptor.java
@@ -1,5 +1,6 @@
 package ru.tinkoff.kora.validation.module.http.server;
 
+import jakarta.annotation.Nullable;
 import ru.tinkoff.kora.common.Context;
 import ru.tinkoff.kora.http.server.common.HttpServerInterceptor;
 import ru.tinkoff.kora.http.server.common.HttpServerRequest;
@@ -7,12 +8,12 @@ import ru.tinkoff.kora.http.server.common.HttpServerResponse;
 import ru.tinkoff.kora.http.server.common.HttpServerResponseException;
 import ru.tinkoff.kora.validation.common.ViolationException;
 
-import jakarta.annotation.Nullable;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
 import java.util.concurrent.CompletionStage;
 
 public final class ValidationHttpServerInterceptor implements HttpServerInterceptor {
+
     @Nullable
     private final ViolationExceptionHttpServerResponseMapper mapper;
 
@@ -25,25 +26,26 @@ public final class ValidationHttpServerInterceptor implements HttpServerIntercep
         try {
             return chain.process(context, request).exceptionallyCompose(e -> {
                 if (e instanceof ViolationException ve) {
-                    return CompletableFuture.completedFuture(toResponse(request, ve));
+                    return CompletableFuture.completedFuture(toResponse(context, request, ve));
                 } else if (e instanceof CompletionException ce && ce.getCause() instanceof ViolationException ve) {
-                    return CompletableFuture.completedFuture(toResponse(request, ve));
+                    return CompletableFuture.completedFuture(toResponse(context, request, ve));
                 } else {
                     return CompletableFuture.failedFuture(e);
                 }
             });
         } catch (CompletionException ce) {
             if (ce.getCause() instanceof ViolationException ve) {
-                return CompletableFuture.completedFuture(toResponse(request, ve));
+                return CompletableFuture.completedFuture(toResponse(context, request, ve));
             }
             throw ce;
         } catch (ViolationException e) {
-            return CompletableFuture.completedFuture(toResponse(request, e));
+            return CompletableFuture.completedFuture(toResponse(context, request, e));
         }
     }
 
-    private HttpServerResponse toResponse(HttpServerRequest request, ViolationException exception) {
+    private HttpServerResponse toResponse(Context context, HttpServerRequest request, ViolationException exception) {
         if (this.mapper != null) {
+            context.inject();
             var response = this.mapper.apply(request, exception);
             if (response != null) {
                 return response;
