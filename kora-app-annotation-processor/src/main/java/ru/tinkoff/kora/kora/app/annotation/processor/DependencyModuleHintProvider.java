5563e81f0aa966664f7902790d4ec79d0b9cf673
Anton Kurako <goodforgod.dev@gmail.com>
2025-03-19 17:26:08 +0300

    Cleanup

--- kora-app-annotation-processor/src/main/java/ru/tinkoff/kora/kora/app/annotation/processor/DependencyModuleHintProvider.java
+++ kora-app-annotation-processor/src/main/java/ru/tinkoff/kora/kora/app/annotation/processor/DependencyModuleHintProvider.java
@@ -44,15 +44,9 @@ public class DependencyModuleHintProvider {
 
         record HintWithTag(TypeMirror type, String artifact, String module, Set<String> tags) implements DependencyModuleHintProvider.Hint {
             public String message() {
-                if (tags.equals(Set.of("ru.tinkoff.kora.json.common.annotation.Json"))) {
-                    return "Missing component of type %s which can be provided by kora module, you may forgot to plug it\nModule class: %s (required tag: `@Json`)\nArtifact dependency: %s\n".formatted(
-                        type, module, artifact
-                    );
-                } else {
-                    return "Missing component of type %s which can be provided by kora module, you may forgot to plug it\nModule class: %s (required tags: `@Tag(%s)`)\nArtifact dependency: %s\n".formatted(
-                        type, module, tags.stream().map(s -> s + ".class").collect(Collectors.joining(", ", "{", "}")), artifact
-                    );
-                }
+                return "Missing component of type %s which can be provided by kora module, you may forgot to plug it\nModule class: %s (required tags: `@Tag(%s)`)\nArtifact dependency: %s\n".formatted(
+                    type, module, tags.stream().map(s -> s + ".class").collect(Collectors.joining(", ", "{", "}")), artifact
+                );
             }
         }
     }
