215c2d6165a4eca8879e2ec4508a713532ff0b91
Anton Kurako <goodforgod.dev@gmail.com>
2025-03-28 13:58:59 +0300

    Inject full Graph with mocked parts and not consider mocks as roots when only mocks are specified

---
+++ test/test-junit5/src/test/java/ru/tinkoff/kora/test/extension/junit5/mockito/MockAndFullGraphTests.java
@@ -0,0 +1,32 @@
+package ru.tinkoff.kora.test.extension.junit5.mockito;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import ru.tinkoff.kora.application.graph.TypeRef;
+import ru.tinkoff.kora.test.extension.junit5.KoraAppGraph;
+import ru.tinkoff.kora.test.extension.junit5.KoraAppTest;
+import ru.tinkoff.kora.test.extension.junit5.TestComponent;
+import ru.tinkoff.kora.test.extension.junit5.testdata.GenericComponent;
+import ru.tinkoff.kora.test.extension.junit5.testdata.TestApplication;
+import ru.tinkoff.kora.test.extension.junit5.testdata.TestApplication.CustomWrapper;
+import ru.tinkoff.kora.test.extension.junit5.testdata.TestComponent1;
+import ru.tinkoff.kora.test.extension.junit5.testdata.TestComponent3;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@KoraAppTest(TestApplication.class)
+public class MockAndFullGraphTests {
+
+    @TestComponent
+    private KoraAppGraph graph;
+
+    @Mock
+    @TestComponent
+    private TestComponent3 mock;
+
+    @Test
+    void graphIsFull() {
+        assertNotNull(graph.getFirst(TypeRef.of(GenericComponent.class, String.class)));
+        assertNotNull(graph.getFirst(CustomWrapper.class));
+    }
+}
