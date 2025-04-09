c1094d808346a4a1c2b9e6bf13c09dfb4d8ebe1e
Anton Duyun <anton.duyun@gmail.com>
2025-04-09 15:38:48 +0300

    tmp

--- json/json-annotation-processor/src/test/java/ru/tinkoff/kora/json/annotation/processor/extension/JsonKoraExtensionTest.java
+++ json/json-annotation-processor/src/test/java/ru/tinkoff/kora/json/annotation/processor/extension/JsonKoraExtensionTest.java
@@ -1,13 +1,15 @@
 package ru.tinkoff.kora.json.annotation.processor.extension;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.tinkoff.kora.json.annotation.processor.AbstractJsonAnnotationProcessorTest;
+import ru.tinkoff.kora.json.annotation.processor.JsonAnnotationProcessor;
 import ru.tinkoff.kora.kora.app.annotation.processor.KoraAppProcessor;
 
 import javax.tools.Diagnostic;
 import java.util.List;
 import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class JsonKoraExtensionTest extends AbstractJsonAnnotationProcessorTest {
 
@@ -17,18 +19,51 @@ class JsonKoraExtensionTest extends AbstractJsonAnnotationProcessorTest {
             @ru.tinkoff.kora.common.KoraApp
             public interface TestApp {
               interface TestInterface {}
-
+            
               default String root0(ru.tinkoff.kora.json.common.JsonReader<TestInterface> r) {return "";}
-              
+            
               @Root
               default Integer root1(String test) {return 42;}
             }
             """);
 
-        Assertions.assertThat(compileResult.isFailed()).isTrue();
-        Assertions.assertThat(compileResult.diagnostic()).anyMatch(d -> d.getKind() == Diagnostic.Kind.ERROR
+        assertThat(compileResult.isFailed()).isTrue();
+        assertThat(compileResult.diagnostic()).anyMatch(d -> d.getKind() == Diagnostic.Kind.ERROR
             && d.getMessage(Locale.US).contains("Required dependency type wasn't found and can't be auto created: " +
             "ru.tinkoff.kora.json.common.JsonReader<ru.tinkoff.kora.json.annotation.processor.extension.packageForJsonKoraExtensionTest.testReaderFromExtensionNotFoundForInterface.TestApp.TestInterface>"));
+    }
+
+    @Test
+    public void testReaderFoundForAnnotatedRecord() {
+        compile(List.of(new KoraAppProcessor(), new JsonAnnotationProcessor()), """
+            @ru.tinkoff.kora.common.KoraApp
+            public interface TestApp {
+              @ru.tinkoff.kora.json.common.annotation.Json
+              record TestRecord() {}
+            
+              @Root
+              default Integer root1(ru.tinkoff.kora.json.common.JsonReader<TestRecord> r) {return 42;}
+            }
+            """);
+        compileResult.assertSuccess();
+        var app = loadGraph("TestApp");
+        assertThat(app.draw().getNodes()).hasSize(2);
+    }
+
+    @Test
+    public void testReaderFoundForNotAnnotatedRecord() {
+        compile(List.of(new KoraAppProcessor(), new JsonAnnotationProcessor()), """
+            @ru.tinkoff.kora.common.KoraApp
+            public interface TestApp {
+              record TestRecord() {}
+            
+              @Root
+              default Integer root1(ru.tinkoff.kora.json.common.JsonReader<TestRecord> r) {return 42;}
+            }
+            """);
+        compileResult.assertSuccess();
+        var app = loadGraph("TestApp");
+        assertThat(app.draw().getNodes()).hasSize(2);
     }
 
     @Test
@@ -37,15 +72,49 @@ class JsonKoraExtensionTest extends AbstractJsonAnnotationProcessorTest {
             @ru.tinkoff.kora.common.KoraApp
             public interface TestApp {
               interface TestInterface {}
-
+            
               @Root
               default String root(ru.tinkoff.kora.json.common.JsonWriter<TestInterface> r) {return "";}
             }
             """);
 
-        Assertions.assertThat(compileResult.isFailed()).isTrue();
-        Assertions.assertThat(compileResult.diagnostic()).anyMatch(d -> d.getKind() == Diagnostic.Kind.ERROR
+        assertThat(compileResult.isFailed()).isTrue();
+        assertThat(compileResult.diagnostic()).anyMatch(d -> d.getKind() == Diagnostic.Kind.ERROR
             && d.getMessage(Locale.US).contains("Required dependency type wasn't found and can't be auto created: " +
             "ru.tinkoff.kora.json.common.JsonWriter<ru.tinkoff.kora.json.annotation.processor.extension.packageForJsonKoraExtensionTest.testWriterFromExtensionNotFoundForInterface.TestApp.TestInterface>"));
     }
+
+    @Test
+    public void testWriterFoundForAnnotatedRecord() {
+        compile(List.of(new KoraAppProcessor(), new JsonAnnotationProcessor()), """
+            @ru.tinkoff.kora.common.KoraApp
+            public interface TestApp {
+              @ru.tinkoff.kora.json.common.annotation.Json
+              record TestRecord() {}
+            
+              @Root
+              default Integer root1(ru.tinkoff.kora.json.common.JsonWriter<TestRecord> r) {return 42;}
+            }
+            """);
+        compileResult.assertSuccess();
+        var app = loadGraph("TestApp");
+        assertThat(app.draw().getNodes()).hasSize(2);
+    }
+
+    @Test
+    public void testWriterFoundForNotAnnotatedRecord() {
+        compile(List.of(new KoraAppProcessor(), new JsonAnnotationProcessor()), """
+            @ru.tinkoff.kora.common.KoraApp
+            public interface TestApp {
+              record TestRecord() {}
+            
+              @Root
+              default Integer root1(ru.tinkoff.kora.json.common.JsonWriter<TestRecord> r) {return 42;}
+            }
+            """);
+        compileResult.assertSuccess();
+        var app = loadGraph("TestApp");
+        assertThat(app.draw().getNodes()).hasSize(2);
+    }
+
 }
