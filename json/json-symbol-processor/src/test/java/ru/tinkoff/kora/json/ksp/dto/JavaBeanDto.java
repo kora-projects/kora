3ed65e7d11e50eb89bbd0b3c8749c5175f0e9f52
Anton Duyun <anton.duyun@gmail.com>
2025-04-11 18:05:39 +0300

    Fix ksp generation of json writers for java classes (#298)

---
+++ json/json-symbol-processor/src/test/java/ru/tinkoff/kora/json/ksp/dto/JavaBeanDto.java
@@ -0,0 +1,15 @@
+package ru.tinkoff.kora.json.ksp.dto;
+
+public class JavaBeanDto {
+    private java.lang.String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public JavaBeanDto() {}
+}
---
+++ json/json-symbol-processor/src/test/kotlin/ru/tinkoff/kora/json/ksp/extension/JsonKoraExtensionTest.kt
@@ -0,0 +1,186 @@
+package ru.tinkoff.kora.json.ksp.extension
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import ru.tinkoff.kora.application.graph.ApplicationGraphDraw
+import ru.tinkoff.kora.json.ksp.JsonSymbolProcessorProvider
+import ru.tinkoff.kora.kora.app.ksp.KoraAppProcessorProvider
+import ru.tinkoff.kora.ksp.common.AbstractSymbolProcessorTest
+
+class JsonKoraExtensionTest : AbstractSymbolProcessorTest() {
+    @Test
+    fun testReaderFromAnnotatedClass() {
+        compile0(listOf(KoraAppProcessorProvider(), JsonSymbolProcessorProvider()), """
+            @KoraApp
+            interface TestApp {
+                @ru.tinkoff.kora.json.common.annotation.Json
+                data class TestClass(val a: String)
+
+                @Root
+                fun test(r: ru.tinkoff.kora.json.common.JsonReader<TestClass>) {}
+            }
+        """.trimIndent())
+
+        compileResult.assertSuccess()
+        val graph = newObject("TestAppGraph").invoke<ApplicationGraphDraw>("graph")!!
+        assertThat(graph.nodes).hasSize(2)
+    }
+
+    @Test
+    fun testReaderFromNonAnnotatedClass() {
+        compile0(listOf(KoraAppProcessorProvider(), JsonSymbolProcessorProvider()), """
+            @KoraApp
+            interface TestApp  : ru.tinkoff.kora.json.common.JsonCommonModule{
+                data class TestClass(val a: String)
+
+                @Root
+                fun test(r: ru.tinkoff.kora.json.common.JsonReader<ru.tinkoff.kora.json.ksp.dto.JavaBeanDto>) {}
+            }
+        """.trimIndent())
+
+        compileResult.assertSuccess()
+        val graph = newObject("TestAppGraph").invoke<ApplicationGraphDraw>("graph")!!
+        assertThat(graph.nodes).hasSize(2)
+    }
+
+    @Test
+    fun testWriterFromAnnotatedClass() {
+        compile0(listOf(KoraAppProcessorProvider(), JsonSymbolProcessorProvider()), """
+            @KoraApp
+            interface TestApp {
+                @ru.tinkoff.kora.json.common.annotation.Json
+                data class TestClass(val a: String)
+
+                @Root
+                fun test(r: ru.tinkoff.kora.json.common.JsonWriter<TestClass>) {}
+            }
+        """.trimIndent())
+
+        compileResult.assertSuccess()
+        val graph = newObject("TestAppGraph").invoke<ApplicationGraphDraw>("graph")!!
+        assertThat(graph.nodes).hasSize(2)
+    }
+
+    @Test
+    fun testWriterFromNonAnnotatedClass() {
+        compile0(listOf(KoraAppProcessorProvider(), JsonSymbolProcessorProvider()), """
+            @KoraApp
+            interface TestApp {
+                data class TestClass(val a: String)
+
+                @Root
+                fun test(r: ru.tinkoff.kora.json.common.JsonWriter<TestClass>) {}
+            }
+        """.trimIndent())
+
+        compileResult.assertSuccess()
+        val graph = newObject("TestAppGraph").invoke<ApplicationGraphDraw>("graph")!!
+        assertThat(graph.nodes).hasSize(2)
+    }
+
+    @Test
+    fun testReaderFromAnnotatedEnum() {
+        compile0(listOf(KoraAppProcessorProvider(), JsonSymbolProcessorProvider()), """
+            @KoraApp
+            interface TestApp : ru.tinkoff.kora.json.common.JsonCommonModule {
+                @ru.tinkoff.kora.json.common.annotation.Json
+                enum class TestEnum { INSTANCE }
+
+                @Root
+                fun test(r: ru.tinkoff.kora.json.common.JsonReader<TestEnum>) {}
+            }
+        """.trimIndent())
+
+        compileResult.assertSuccess()
+        val graph = newObject("TestAppGraph").invoke<ApplicationGraphDraw>("graph")!!
+        assertThat(graph.nodes).hasSize(3)
+    }
+
+    @Test
+    fun testReaderFromNonAnnotatedEnum() {
+        compile0(listOf(KoraAppProcessorProvider(), JsonSymbolProcessorProvider()), """
+            @KoraApp
+            interface TestApp : ru.tinkoff.kora.json.common.JsonCommonModule {
+                enum class TestEnum { INSTANCE }
+
+                @Root
+                fun test(r: ru.tinkoff.kora.json.common.JsonReader<TestEnum>) {}
+            }
+        """.trimIndent())
+
+        compileResult.assertSuccess()
+        val graph = newObject("TestAppGraph").invoke<ApplicationGraphDraw>("graph")!!
+        assertThat(graph.nodes).hasSize(3)
+    }
+
+    @Test
+    fun testWriterFromAnnotatedEnum() {
+        compile0(listOf(KoraAppProcessorProvider(), JsonSymbolProcessorProvider()), """
+            @KoraApp
+            interface TestApp : ru.tinkoff.kora.json.common.JsonCommonModule {
+                @ru.tinkoff.kora.json.common.annotation.Json
+                enum class TestEnum { INSTANCE }
+
+                @Root
+                fun test(r: ru.tinkoff.kora.json.common.JsonWriter<TestEnum>) {}
+            }
+        """.trimIndent())
+
+        compileResult.assertSuccess()
+        val graph = newObject("TestAppGraph").invoke<ApplicationGraphDraw>("graph")!!
+        assertThat(graph.nodes).hasSize(3)
+    }
+
+    @Test
+    fun testWriterFromNonAnnotatedEnum() {
+        compile0(listOf(KoraAppProcessorProvider(), JsonSymbolProcessorProvider()), """
+            @KoraApp
+            interface TestApp : ru.tinkoff.kora.json.common.JsonCommonModule {
+                enum class TestEnum { INSTANCE }
+
+                @Root
+                fun test(r: ru.tinkoff.kora.json.common.JsonWriter<TestEnum>) {}
+            }
+        """.trimIndent())
+
+        compileResult.assertSuccess()
+        val graph = newObject("TestAppGraph").invoke<ApplicationGraphDraw>("graph")!!
+        assertThat(graph.nodes).hasSize(3)
+    }
+
+
+    @Test
+    fun testReaderFromJavaClass() {
+        compile0(listOf(KoraAppProcessorProvider(), JsonSymbolProcessorProvider()), """
+            @KoraApp
+            interface TestApp  : ru.tinkoff.kora.json.common.JsonCommonModule{
+                data class TestClass(val a: String)
+
+                @Root
+                fun test(r: ru.tinkoff.kora.json.common.JsonReader<ru.tinkoff.kora.json.ksp.dto.JavaBeanDto>) {}
+            }
+        """.trimIndent())
+
+        compileResult.assertSuccess()
+        val graph = newObject("TestAppGraph").invoke<ApplicationGraphDraw>("graph")!!
+        assertThat(graph.nodes).hasSize(2)
+    }
+
+    @Test
+    fun testWriterFromJavaClass() {
+        compile0(listOf(KoraAppProcessorProvider(), JsonSymbolProcessorProvider()), """
+            @KoraApp
+            interface TestApp  : ru.tinkoff.kora.json.common.JsonCommonModule{
+                data class TestClass(val a: String)
+
+                @Root
+                fun test(r: ru.tinkoff.kora.json.common.JsonWriter<ru.tinkoff.kora.json.ksp.dto.JavaBeanDto>) {}
+            }
+        """.trimIndent())
+
+        compileResult.assertSuccess()
+        val graph = newObject("TestAppGraph").invoke<ApplicationGraphDraw>("graph")!!
+        assertThat(graph.nodes).hasSize(2)
+    }
+
+}
