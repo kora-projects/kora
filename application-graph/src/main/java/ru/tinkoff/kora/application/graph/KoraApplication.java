d7a8afa6ee508b207ea80f0ee11369a1080d413d
Anton Kurako <goodforgod.dev@gmail.com>
2025-04-28 16:04:33 +0300

    KoraCircuitBreaker half-open count ALL errors introduced in 1.1.24 and stuck fixed KoraRetry and KoraCircuitBreaker logging improved KoraCircuitBreakerTests reinforced

--- application-graph/src/main/java/ru/tinkoff/kora/application/graph/KoraApplication.java
+++ application-graph/src/main/java/ru/tinkoff/kora/application/graph/KoraApplication.java
@@ -38,7 +38,6 @@ public final class KoraApplication {
                 }
             });
             thread.setName("kora-shutdown");
-            thread.setDaemon(true);
             Runtime.getRuntime().addShutdownHook(thread);
             return graph;
         } catch (Exception e) {
