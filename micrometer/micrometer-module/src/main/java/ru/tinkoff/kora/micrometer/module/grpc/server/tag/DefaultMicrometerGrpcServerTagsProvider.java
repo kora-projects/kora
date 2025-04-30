aed0b5d7fde4ef024fdfebe511767cb105419be6
e.murzakaev <e.murzakaev@tinkoff.ru>
2025-04-30 21:35:22 +0300

    micrometer gRPC server tags provider added

--- micrometer/micrometer-module/src/main/java/ru/tinkoff/kora/micrometer/module/grpc/server/tag/DefaultMicrometerGrpcServerTagsProvider.java
+++ micrometer/micrometer-module/src/main/java/ru/tinkoff/kora/micrometer/module/grpc/server/tag/DefaultMicrometerGrpcServerTagsProvider.java
@@ -3,35 +3,39 @@ package ru.tinkoff.kora.micrometer.module.grpc.server.tag;
 import io.micrometer.core.instrument.Tag;
 import io.opentelemetry.semconv.incubating.RpcIncubatingAttributes;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DefaultMicrometerGrpcServerTagsProvider implements MicrometerGrpcServerTagsProvider {
 
     @Override
-    public Iterable<Tag> getRequestsTags(MetricsKey key) {
-        return List.of(
+    public List<Tag> getRequestsTags(MetricsKey key) {
+        var tags = List.of(
             Tag.of(RpcIncubatingAttributes.RPC_SYSTEM.getKey(), RpcIncubatingAttributes.RpcSystemIncubatingValues.GRPC),
             Tag.of(RpcIncubatingAttributes.RPC_SERVICE.getKey(), key.serviceName()),
             Tag.of(RpcIncubatingAttributes.RPC_METHOD.getKey(), key.methodName())
         );
+        return new ArrayList<>(tags);
     }
 
     @Override
-    public Iterable<Tag> getResponsesTags(MetricsKey key) {
-        return List.of(
+    public List<Tag> getResponsesTags(MetricsKey key) {
+        var tags = List.of(
             Tag.of(RpcIncubatingAttributes.RPC_SYSTEM.getKey(), RpcIncubatingAttributes.RpcSystemIncubatingValues.GRPC),
             Tag.of(RpcIncubatingAttributes.RPC_SERVICE.getKey(), key.serviceName()),
             Tag.of(RpcIncubatingAttributes.RPC_METHOD.getKey(), key.methodName())
         );
+        return new ArrayList<>(tags);
     }
 
     @Override
-    public Iterable<Tag> getDurationTags(Integer code, MetricsKey key) {
-        return List.of(
+    public List<Tag> getDurationTags(Integer code, MetricsKey key) {
+        var tags = List.of(
             Tag.of(RpcIncubatingAttributes.RPC_SYSTEM.getKey(), RpcIncubatingAttributes.RpcSystemIncubatingValues.GRPC),
             Tag.of(RpcIncubatingAttributes.RPC_SERVICE.getKey(), key.serviceName()),
             Tag.of(RpcIncubatingAttributes.RPC_METHOD.getKey(), key.methodName()),
             Tag.of(RpcIncubatingAttributes.RPC_GRPC_STATUS_CODE.getKey(), Integer.toString(code))
         );
+        return new ArrayList<>(tags);
     }
 }
