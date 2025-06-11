a027626ea08f0de2740b5e0c748f9b2337a8bafc
DmitriiPl <Plighig@mail.ru>
2025-06-11 08:51:16 +0300

    Kora JUnit mockito session & report unused stubbings * add mock reporter tests

---
+++ test/test-junit5/src/test/java/ru/tinkoff/kora/test/extension/junit5/mockito/MockDefaultStubbingReporterTests.java
@@ -0,0 +1,53 @@
+package ru.tinkoff.kora.test.extension.junit5.mockito;
+
+import org.junit.jupiter.api.Test;
+import org.junit.platform.engine.discovery.DiscoverySelectors;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import ru.tinkoff.kora.test.extension.junit5.KoraAppTest;
+import ru.tinkoff.kora.test.extension.junit5.TestComponent;
+import ru.tinkoff.kora.test.extension.junit5.testdata.TestApplication;
+import ru.tinkoff.kora.test.extension.junit5.testdata.TestComponent1;
+import ru.tinkoff.kora.test.extension.junit5.testdata.TestComponent12;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SuppressWarnings(value = {"JUnitMalformedDeclaration", "EqualsWithItself"})
+public class MockDefaultStubbingReporterTests {
+
+    @Test
+    public void mockDefault() {
+        var request = LauncherDiscoveryRequestBuilder.request()
+            .selectors(DiscoverySelectors.selectClass(MockDefaultTest.class)).build();
+
+        var launcher = LauncherFactory.create();
+        var listener = new SummaryGeneratingListener();
+
+        launcher.registerTestExecutionListeners(listener);
+        launcher.execute(request);
+
+        assertEquals(2, listener.getSummary().getTestsFailedCount());
+    }
+
+    @KoraAppTest(value = TestApplication.class, components = TestComponent12.class)
+    public static class MockDefaultTest {
+        @Mock
+        @TestComponent
+        private TestComponent1 mock;
+
+        @Test
+        void mockField() {
+            Mockito.when(mock.get()).thenReturn("?");
+            assertEquals("?", "?");
+        }
+
+        @Test
+        void mockParameter(@Mock @TestComponent TestComponent1 mock) {
+            Mockito.when(mock.get()).thenReturn("?");
+            assertEquals("?", "?");
+        }
+    }
+}
