f56913b0ed57f2dcbecc7c8864afc04e92fec415
DmitriiPl <Plighig@mail.ru>
2025-07-17 16:41:29 +0300

    Kora JUnit mockito session & report unused stubbings * fix javadoc

--- test/test-junit5/src/main/java/ru/tinkoff/kora/test/extension/junit5/mockito/MockitoStrictness.java
+++ test/test-junit5/src/main/java/ru/tinkoff/kora/test/extension/junit5/mockito/MockitoStrictness.java
@@ -11,7 +11,6 @@ import java.lang.annotation.*;
  * within a test class. It allows developers to define how strictly Mockito should enforce the behavior
  * of mocks, such as verifying stubbed methods and detecting unused stubs or unexpected interactions.
  * </p>
- * <p>
  * This annotation can be applied to a test class to set the default {@link Strictness} level for all
  * mocks created by Mockito in that class. The strictness level can be one of the values defined in
  * {@link Strictness}, with the default being {@link Strictness#STRICT_STUBS}.
@@ -22,7 +21,7 @@ import java.lang.annotation.*;
  *     <li>{@link Strictness#LENIENT}: Allows unstubbed calls and does not report unused stubs.</li>
  *     <li>{@link Strictness#WARN}: Warns about potential issues without failing the test.</li>
  * </ul>
- * </p>
+ *
  *
  * @see Strictness
  */
