d8cc4054aaff7b11a0b13e889dacba9b8cee30a3
DmitriiPl <Plighig@mail.ru>
2025-07-17 15:22:23 +0300

    Kora JUnit mockito session & report unused stubbings * review fixes

--- test/test-junit5/src/test/java/ru/tinkoff/kora/test/extension/junit5/mockito/MockStrictStubbingReporterTests.java
+++ test/test-junit5/src/test/java/ru/tinkoff/kora/test/extension/junit5/mockito/MockStrictStubbingReporterTests.java
@@ -10,7 +10,6 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.quality.Strictness;
 import ru.tinkoff.kora.test.extension.junit5.KoraAppTest;
-import ru.tinkoff.kora.test.extension.junit5.MockStrictness;
 import ru.tinkoff.kora.test.extension.junit5.TestComponent;
 import ru.tinkoff.kora.test.extension.junit5.testdata.TestApplication;
 import ru.tinkoff.kora.test.extension.junit5.testdata.TestComponent1;
@@ -38,7 +37,7 @@ public class MockStrictStubbingReporterTests {
 
     @Disabled
     @KoraAppTest(value = TestApplication.class, components = TestComponent12.class)
-    @MockStrictness(Strictness.STRICT_STUBS)
+    @MockitoStrictness(Strictness.STRICT_STUBS)
     static class MockStrictTest {
         @Mock
         @TestComponent
