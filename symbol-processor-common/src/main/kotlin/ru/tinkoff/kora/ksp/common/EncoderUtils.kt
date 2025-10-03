04953746d3e4f44dc610c3927487ef5bcc98ce68
mi.bogdanov <mi.bogdanov@tbank.ru>
2025-10-03 12:46:13 +0300

    fix

--- symbol-processor-common/src/main/kotlin/ru/tinkoff/kora/ksp/common/EncoderUtils.kt
+++ symbol-processor-common/src/main/kotlin/ru/tinkoff/kora/ksp/common/EncoderUtils.kt
@@ -8,12 +8,12 @@ object EncoderUtils {
     /**
      * @param s {@code String} to be translated.
      * @param charset the given charset
-     * @param encodeSpaceToPlus encode space as + char
+     * @param disableEncodeSpaceToPlus disable encoding space as + char
      * @return  the translated {@code String}.
      */
-    fun encode(s: String, charset: Charset, escapeSpaceToPlus: Boolean): String {
+    fun encode(s: String, charset: Charset, disableEncodeSpaceToPlus: Boolean): String {
         var encoded = URLEncoder.encode(s, charset)
-        if (!escapeSpaceToPlus) {
+        if (disableEncodeSpaceToPlus) {
             encoded = encoded.replace("+", "%20")
         }
         return encoded
