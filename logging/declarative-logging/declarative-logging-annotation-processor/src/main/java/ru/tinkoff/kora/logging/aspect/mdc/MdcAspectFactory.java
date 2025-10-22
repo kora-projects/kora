805e2b902e18169b70385e5293675b39078de45c
Dmitry Sadchikov <dmi.sadchikov@gmail.com>
2025-10-22 18:01:42 +0300

    Fix dependency

--- logging/declarative-logging/declarative-logging-annotation-processor/src/main/java/ru/tinkoff/kora/logging/aspect/mdc/MdcAspectFactory.java
+++ logging/declarative-logging/declarative-logging-annotation-processor/src/main/java/ru/tinkoff/kora/logging/aspect/mdc/MdcAspectFactory.java
@@ -2,12 +2,10 @@ package ru.tinkoff.kora.logging.aspect.mdc;
 
 import ru.tinkoff.kora.aop.annotation.processor.KoraAspect;
 import ru.tinkoff.kora.aop.annotation.processor.KoraAspectFactory;
-import ru.tinkoff.kora.common.Component;
 
 import javax.annotation.processing.ProcessingEnvironment;
 import java.util.Optional;
 
-@Component
 public class MdcAspectFactory implements KoraAspectFactory {
 
     @Override
