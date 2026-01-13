037583aeb91d45423d06c0841f8b727864ddd745
Anton Duyun <anton.duyun@gmail.com>
2026-01-13 20:13:14 +0300

    Database telemetry should not require tracer

--- database/database-common/src/main/java/ru/tinkoff/kora/database/common/telemetry/DefaultDataBaseTelemetryFactory.java
+++ database/database-common/src/main/java/ru/tinkoff/kora/database/common/telemetry/DefaultDataBaseTelemetryFactory.java
@@ -1,20 +1,29 @@
 package ru.tinkoff.kora.database.common.telemetry;
 
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.composite.CompositeMeterRegistry;
 import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.api.trace.TracerProvider;
+import org.jspecify.annotations.Nullable;
 import org.slf4j.LoggerFactory;
 
+import java.util.Objects;
+
 public class DefaultDataBaseTelemetryFactory implements DataBaseTelemetryFactory {
+    @Nullable
     private final Tracer tracer;
+    @Nullable
     private final MeterRegistry meterRegistry;
 
-    public DefaultDataBaseTelemetryFactory(Tracer tracer, MeterRegistry meterRegistry) {
+    public DefaultDataBaseTelemetryFactory(@Nullable Tracer tracer, @Nullable MeterRegistry meterRegistry) {
         this.tracer = tracer;
         this.meterRegistry = meterRegistry;
     }
 
     @Override
     public DataBaseTelemetry get(DatabaseTelemetryConfig config, String name, String dbType) {
+        var tracer = Objects.requireNonNullElse(this.tracer, TracerProvider.noop().get("database"));
+        var meterRegistry = Objects.requireNonNullElse(this.meterRegistry, new CompositeMeterRegistry());
 
         return new DefaultDataBaseTelemetry(config, name, dbType, tracer, meterRegistry, LoggerFactory.getLogger("ru.tinkoff.kora.database." + name + ".query"));
     }
