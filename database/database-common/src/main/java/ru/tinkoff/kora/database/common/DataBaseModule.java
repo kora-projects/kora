1224d76333e711b1437a969f3749184bc162b15f
Anton Duyun <anton.duyun@gmail.com>
2026-01-13 20:27:44 +0300

    Database telemetry should not require tracer (#560)

--- database/database-common/src/main/java/ru/tinkoff/kora/database/common/DataBaseModule.java
+++ database/database-common/src/main/java/ru/tinkoff/kora/database/common/DataBaseModule.java
@@ -1,18 +1,15 @@
 package ru.tinkoff.kora.database.common;
 
 import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.composite.CompositeMeterRegistry;
 import io.opentelemetry.api.trace.Tracer;
 import org.jspecify.annotations.Nullable;
 import ru.tinkoff.kora.common.DefaultComponent;
 import ru.tinkoff.kora.database.common.telemetry.DefaultDataBaseTelemetryFactory;
 
-import java.util.Objects;
-
 public interface DataBaseModule {
 
     @DefaultComponent
-    default DefaultDataBaseTelemetryFactory defaultDataBaseTelemetry(Tracer tracer, @Nullable MeterRegistry meterRegistry) {
-        return new DefaultDataBaseTelemetryFactory(tracer, Objects.requireNonNullElse(meterRegistry, new CompositeMeterRegistry()));
+    default DefaultDataBaseTelemetryFactory defaultDataBaseTelemetry(@Nullable Tracer tracer, @Nullable MeterRegistry meterRegistry) {
+        return new DefaultDataBaseTelemetryFactory(tracer, meterRegistry);
     }
 }
