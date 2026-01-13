4882c5e6dc21c618f04525c9c0a3b43a0a8bdaff
Anton Duyun <anton.duyun@gmail.com>
2026-01-13 20:28:23 +0300

    Add `@Module` on generated s3 client modules

--- experimental/s3-client/src/main/java/ru/tinkoff/kora/s3/client/telemetry/S3ClientTelemetryConfig.java
+++ experimental/s3-client/src/main/java/ru/tinkoff/kora/s3/client/telemetry/S3ClientTelemetryConfig.java
@@ -1,7 +1,9 @@
 package ru.tinkoff.kora.s3.client.telemetry;
 
+import ru.tinkoff.kora.config.common.annotation.ConfigValueExtractor;
 import ru.tinkoff.kora.telemetry.common.TelemetryConfig;
 
+@ConfigValueExtractor
 public interface S3ClientTelemetryConfig extends TelemetryConfig {
     @Override
     S3ClientLogConfig logging();
@@ -12,14 +14,17 @@ public interface S3ClientTelemetryConfig extends TelemetryConfig {
     @Override
     S3ClientTracingConfig tracing();
 
+    @ConfigValueExtractor
     interface S3ClientLogConfig extends TelemetryConfig.LogConfig {
 
     }
 
+    @ConfigValueExtractor
     interface S3ClientTracingConfig extends TelemetryConfig.TracingConfig {
 
     }
 
+    @ConfigValueExtractor
     interface S3ClientMetricsConfig extends TelemetryConfig.MetricsConfig {
 
     }
