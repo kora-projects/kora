40a502334992454f1af08aac3a792ba222af17b1
Anton Duyun <anton.duyun@gmail.com>
2026-01-19 16:02:32 +0300

    Prevent connection leaking in http client telemetry interceptor (#567)

--- http/http-client-common/src/main/java/ru/tinkoff/kora/http/client/common/interceptor/TelemetryInterceptor.java
+++ http/http-client-common/src/main/java/ru/tinkoff/kora/http/client/common/interceptor/TelemetryInterceptor.java
@@ -5,7 +5,11 @@ import ru.tinkoff.kora.http.client.common.request.HttpClientRequest;
 import ru.tinkoff.kora.http.client.common.response.HttpClientResponse;
 import ru.tinkoff.kora.http.client.common.telemetry.HttpClientTelemetry;
 
+import java.io.IOException;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class TelemetryInterceptor implements HttpClientInterceptor {
 
@@ -27,27 +31,45 @@ public class TelemetryInterceptor implements HttpClientInterceptor {
                 return chain.process(ctx, request);
             }
             fork.inject();
-            return chain.process(fork, telemetryContext.request())
-                .whenComplete((rs, error) -> {
-                    if (error != null) {
-                        var old = Context.current();
-                        try {
-                            fork.inject();
-                            telemetryContext.close(null, error);
-                        } finally {
-                            old.inject();
+            var future = new CompletableFuture<HttpClientResponse>();
+            var cancelled = new AtomicReference<CancellationException>();
+            future.whenComplete((ignored, throwable) -> {
+                if (throwable instanceof CancellationException ce) {
+                    cancelled.set(ce);
+                }
+            });
+            chain.process(fork, telemetryContext.request()).whenComplete((rs, error) -> {
+                var old = Context.current();
+                try {
+                    fork.inject();
+                    var cancellation = cancelled.get();
+                    if (cancellation != null) {
+                        if (rs != null) {
+                            try {
+                                rs.close();
+                            } catch (IOException io) {
+                                cancellation.addSuppressed(io);
+                            }
                         }
+                        if (error != null) {
+                            cancellation.addSuppressed(error);
+                        }
+                        var processedRs = telemetryContext.close(null, cancellation);
+                        assert processedRs == null;
+                        return;
                     }
-                })
-                .thenApply(rs -> {
-                    var old = Context.current();
-                    try {
-                        fork.inject();
-                        return telemetryContext.close(rs, null);
-                    } finally {
-                        old.inject();
+                    if (error != null) {
+                        telemetryContext.close(null, error);
+                        future.completeExceptionally(error);
+                    } else {
+                        var processedRs = telemetryContext.close(rs, null);
+                        future.complete(processedRs);
                     }
-                });
+                } finally {
+                    old.inject();
+                }
+            });
+            return future;
         } finally {
             ctx.inject();
         }
