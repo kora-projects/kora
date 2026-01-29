e7214b7711fdf20ca56bcfe740dad5880390077e
Anton Kurako <goodforgod.dev@gmail.com>
2026-01-29 13:01:45 +0300

    Added support for wrapped components and improved graph handling: - Added processing of Wrapped and ValueOf in the application graph - Improved node search logic in the graph considering type inheritance

--- test/test-junit5/src/test/java/ru/tinkoff/kora/test/extension/junit5/testdata/TestComponent3.java
+++ test/test-junit5/src/test/java/ru/tinkoff/kora/test/extension/junit5/testdata/TestComponent3.java
@@ -1,10 +1,5 @@
 package ru.tinkoff.kora.test.extension.junit5.testdata;
 
-import ru.tinkoff.kora.common.Component;
-import ru.tinkoff.kora.common.annotation.Root;
-
-@Root
-@Component
 public class TestComponent3 implements LifecycleComponent {
 
     public String get() {
@@ -13,6 +8,6 @@ public class TestComponent3 implements LifecycleComponent {
 
     @Override
     public void init() {
-        throw new IllegalStateException("OPS");
+        // do nothing
     }
 }
