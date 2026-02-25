c0002ca04f6383397372f0d3d343f855e3df7085
ᴀɴᴛᴏɴ <GoodforGod@users.noreply.github.com>
2026-02-25 10:18:48 +0300

    Enriched Flyway config (#588)
    * Enriched FlywayConfig for migrations

--- database/database-flyway/src/main/java/ru/tinkoff/kora/database/flyway/FlywayConfig.java
+++ database/database-flyway/src/main/java/ru/tinkoff/kora/database/flyway/FlywayConfig.java
@@ -3,11 +3,32 @@ package ru.tinkoff.kora.database.flyway;
 import ru.tinkoff.kora.config.common.annotation.ConfigValueExtractor;
 
 import java.util.List;
+import java.util.Map;
 
 @ConfigValueExtractor
 public interface FlywayConfig {
 
+    default boolean enabled() {
+        return true;
+    }
+
     default List<String> locations() {
         return List.of("db/migration");
+    }
+
+    default boolean executeInTransaction() {
+        return true;
+    }
+
+    default boolean validateOnMigrate() {
+        return true;
+    }
+
+    default boolean mixed() {
+        return false;
+    }
+
+    default Map<String, String> configurationProperties() {
+        return Map.of();
     }
 }
