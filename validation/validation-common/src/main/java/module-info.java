91139557d7db52ac6f384a31bc86f3cdf3f46a8d
Anton Duyun <anton.duyun@gmail.com>
2026-03-02 22:58:13 +0300

    Fix validation module-info exports

--- validation/validation-common/src/main/java/module-info.java
+++ validation/validation-common/src/main/java/module-info.java
@@ -2,4 +2,8 @@ module kora.validation.common {
     requires transitive kora.common;
     requires static org.jetbrains.annotations;
     requires kora.config.common;
+
+    exports ru.tinkoff.kora.validation.common;
+    exports ru.tinkoff.kora.validation.common.annotation;
+    exports ru.tinkoff.kora.validation.common.constraint.factory;
 }
