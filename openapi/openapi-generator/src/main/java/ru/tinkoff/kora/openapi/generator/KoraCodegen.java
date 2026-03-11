6f2bb80f1af624650fdad84de10853bece88113a
Anton Kurako <goodforgod.dev@gmail.com>
2026-03-11 14:58:18 +0300

    Fixed initialization order

--- openapi/openapi-generator/src/main/java/ru/tinkoff/kora/openapi/generator/KoraCodegen.java
+++ openapi/openapi-generator/src/main/java/ru/tinkoff/kora/openapi/generator/KoraCodegen.java
@@ -410,48 +410,6 @@ public class KoraCodegen extends DefaultCodegen {
         supportsInheritance = true;
         hideGenerationTimestamp = true;
 
-        final Stream<String> languageReservedWords;
-        if (params.codegenMode().isJava()) {
-            languageReservedWords = Stream.of(
-                // Java Reserved Words
-                "abstract", "assert", "boolean", "break", "byte", "case", "catch", "char",
-                "class", "const", "continue", "default", "do", "double", "else", "enum",
-                "extends", "final", "finally", "float", "for", "goto", "if", "implements",
-                "import", "instanceof", "int", "interface", "long", "native", "new", "package",
-                "private", "protected", "public", "return", "short", "static", "strictfp",
-                "super", "switch", "synchronized", "this", "throw", "throws", "transient",
-                "try", "void", "volatile", "while", "var", "record", "yield", "sealed", "null", "when");
-        } else {
-            languageReservedWords = Stream.of(
-                // Kotlin Reserved Words
-                "abstract", "actual", "annotation", "as", "break", "by", "catch", "class",
-                "companion", "const", "constructor", "continue", "data", "do", "dynamic",
-                "else", "enum", "expect", "external", "false", "final", "finally", "for",
-                "fun", "if", "import", "in", "inline", "interface", "internal", "is", "lateinit",
-                "native", "null", "object", "open", "operator", "out", "override", "package",
-                "private", "protected", "public", "reified", "return", "sealed", "set", "super",
-                "suspend", "this", "throw", "true", "try", "typealias", "typeof", "val", "var",
-                "when", "where", "while");
-        }
-
-        setReservedWordsLowerCase(
-            Stream.concat(
-                Stream.of(
-                    // special words
-                    "object",
-                    // used as internal variables, can collide with parameter names
-                    "error",
-                    "localVarPath", "localVarQueryParams", "localVarCollectionQueryParams",
-                    "localVarHeaderParams", "localVarCookieParams", "localVarFormParams", "localVarPostBody",
-                    "localVarAccepts", "localVarAccept", "localVarContentTypes",
-                    "localVarContentType", "localVarAuthNames", "localReturnType"
-                    //  "ApiClient", "ApiException", "ApiResponse", "Configuration", "StringUtil",
-                ),
-                languageReservedWords
-            ).distinct().toList()
-        );
-
-
         instantiationTypes.put("array", "ArrayList");
         instantiationTypes.put("set", "LinkedHashSet");
         instantiationTypes.put("map", "HashMap");
@@ -555,6 +513,47 @@ public class KoraCodegen extends DefaultCodegen {
             }
         }
         this.vendorExtensions.put("allowAspects", params.enableValidation() || !params.additionalContractAnnotations.isEmpty());
+
+        final Stream<String> languageReservedWords;
+        if (params.codegenMode().isJava()) {
+            languageReservedWords = Stream.of(
+                // Java Reserved Words
+                "abstract", "assert", "boolean", "break", "byte", "case", "catch", "char",
+                "class", "const", "continue", "default", "do", "double", "else", "enum",
+                "extends", "final", "finally", "float", "for", "goto", "if", "implements",
+                "import", "instanceof", "int", "interface", "long", "native", "new", "package",
+                "private", "protected", "public", "return", "short", "static", "strictfp",
+                "super", "switch", "synchronized", "this", "throw", "throws", "transient",
+                "try", "void", "volatile", "while", "var", "record", "yield", "sealed", "null", "when");
+        } else {
+            languageReservedWords = Stream.of(
+                // Kotlin Reserved Words
+                "abstract", "actual", "annotation", "as", "break", "by", "catch", "class",
+                "companion", "const", "constructor", "continue", "data", "do", "dynamic",
+                "else", "enum", "expect", "external", "false", "final", "finally", "for",
+                "fun", "if", "import", "in", "inline", "interface", "internal", "is", "lateinit",
+                "native", "null", "object", "open", "operator", "out", "override", "package",
+                "private", "protected", "public", "reified", "return", "sealed", "set", "super",
+                "suspend", "this", "throw", "true", "try", "typealias", "typeof", "val", "var",
+                "when", "where", "while");
+        }
+
+        setReservedWordsLowerCase(
+            Stream.concat(
+                Stream.of(
+                    // special words
+                    "object",
+                    // used as internal variables, can collide with parameter names
+                    "error",
+                    "localVarPath", "localVarQueryParams", "localVarCollectionQueryParams",
+                    "localVarHeaderParams", "localVarCookieParams", "localVarFormParams", "localVarPostBody",
+                    "localVarAccepts", "localVarAccept", "localVarContentTypes",
+                    "localVarContentType", "localVarAuthNames", "localReturnType"
+                    //  "ApiClient", "ApiException", "ApiResponse", "Configuration", "StringUtil",
+                ),
+                languageReservedWords
+            ).distinct().toList()
+        );
 
         embeddedTemplateDir = templateDir = "openapi/templates/kora";
         if (!params.codegenMode.isJava()) {
