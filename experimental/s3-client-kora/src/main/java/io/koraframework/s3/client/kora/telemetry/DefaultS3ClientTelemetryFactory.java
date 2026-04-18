16c2ecb0f91d412d7f984e47b04cded560686be7
Anton Kurako <goodforgod.dev@gmail.com>
2026-04-18 12:44:59 +0300

    Minor cleanup

--- experimental/s3-client-kora/src/main/java/io/koraframework/s3/client/kora/telemetry/DefaultS3ClientTelemetryFactory.java
+++ experimental/s3-client-kora/src/main/java/io/koraframework/s3/client/kora/telemetry/DefaultS3ClientTelemetryFactory.java
@@ -6,13 +6,18 @@ import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.api.trace.TracerProvider;
 import org.jspecify.annotations.Nullable;
 
-public class DefaultS3ClientTelemetryFactory implements S3ClientTelemetryFactory {
+public final class DefaultS3ClientTelemetryFactory implements S3ClientTelemetryFactory {
+
+    private static final Tracer NOOP_TRACER = TracerProvider.noop().get("s3-client-kora-telemetry");
+    private static final MeterRegistry NOOP_METER_REGISTRY = new CompositeMeterRegistry();
+
     @Nullable
     private final Tracer tracer;
     @Nullable
     private final MeterRegistry meterRegistry;
 
-    public DefaultS3ClientTelemetryFactory(@Nullable Tracer tracer, @Nullable MeterRegistry meterRegistry) {
+    public DefaultS3ClientTelemetryFactory(@Nullable Tracer tracer,
+                                           @Nullable MeterRegistry meterRegistry) {
         this.tracer = tracer;
         this.meterRegistry = meterRegistry;
     }
@@ -21,12 +26,12 @@ public class DefaultS3ClientTelemetryFactory implements S3ClientTelemetryFactory
     public S3ClientTelemetry get(S3ClientTelemetryConfig config) {
         var tracerEnabled = this.tracer != null && config.tracing().enabled();
         var metricEnabled = this.meterRegistry != null && config.metrics().enabled();
-        if (!config.logging().enabled() && !metricEnabled && !tracerEnabled) {
+        if (!tracerEnabled && !metricEnabled && !config.logging().enabled()) {
             return NoopS3ClientTelemetry.INSTANCE;
         }
 
-        var tracer = !tracerEnabled ? TracerProvider.noop().get("s3-kora-client-telemetry") : this.tracer;
-        var meterRegistry = !metricEnabled ? new CompositeMeterRegistry() : this.meterRegistry;
+        var tracer = tracerEnabled ? this.tracer : NOOP_TRACER;
+        var meterRegistry = metricEnabled ? this.meterRegistry : NOOP_METER_REGISTRY;
         return new DefaultS3ClientTelemetry(config, tracer, meterRegistry);
     }
 }
