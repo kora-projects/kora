2754e6fc09350fb7a729a1eabf19083704307179
Anton Kurako <goodforgod.dev@gmail.com>
2026-04-18 12:39:34 +0300

    Improve telemetry

--- kafka/kafka/src/main/java/io/koraframework/kafka/common/KafkaModule.java
+++ kafka/kafka/src/main/java/io/koraframework/kafka/common/KafkaModule.java
@@ -1,35 +1,23 @@
 package io.koraframework.kafka.common;
 
-import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.composite.CompositeMeterRegistry;
-import io.opentelemetry.api.trace.Tracer;
-import io.opentelemetry.api.trace.TracerProvider;
-import org.jspecify.annotations.Nullable;
 import io.koraframework.common.DefaultComponent;
 import io.koraframework.kafka.common.consumer.telemetry.DefaultKafkaConsumerTelemetryFactory;
 import io.koraframework.kafka.common.producer.telemetry.DefaultKafkaPublisherTelemetryFactory;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.opentelemetry.api.trace.Tracer;
+import org.jspecify.annotations.Nullable;
 
 public interface KafkaModule extends KafkaDeserializersModule, KafkaSerializersModule {
-    @DefaultComponent
-    default DefaultKafkaConsumerTelemetryFactory defaultKafkaConsumerTelemetryFactory(@Nullable Tracer tracer, @Nullable MeterRegistry meterRegistry) {
-        if (tracer == null) {
-            tracer = TracerProvider.noop().get("kafkaPublisherTelemetry");
-        }
-        if (meterRegistry == null) {
-            meterRegistry = new CompositeMeterRegistry();
-        }
 
+    @DefaultComponent
+    default DefaultKafkaConsumerTelemetryFactory defaultKafkaConsumerTelemetryFactory(@Nullable Tracer tracer,
+                                                                                      @Nullable MeterRegistry meterRegistry) {
         return new DefaultKafkaConsumerTelemetryFactory(tracer, meterRegistry);
     }
 
     @DefaultComponent
-    default DefaultKafkaPublisherTelemetryFactory defaultKafkaProducerTelemetryFactory(@Nullable Tracer tracer, @Nullable MeterRegistry meterRegistry) {
-        if (tracer == null) {
-            tracer = TracerProvider.noop().get("kafkaPublisherTelemetry");
-        }
-        if (meterRegistry == null) {
-            meterRegistry = new CompositeMeterRegistry();
-        }
+    default DefaultKafkaPublisherTelemetryFactory defaultKafkaPublisherTelemetryFactory(@Nullable Tracer tracer,
+                                                                                        @Nullable MeterRegistry meterRegistry) {
         return new DefaultKafkaPublisherTelemetryFactory(tracer, meterRegistry);
     }
 }
--- kafka/kafka/src/main/java/io/koraframework/kafka/common/consumer/telemetry/DefaultKafkaConsumerTelemetryFactory.java
+++ kafka/kafka/src/main/java/io/koraframework/kafka/common/consumer/telemetry/DefaultKafkaConsumerTelemetryFactory.java
@@ -10,22 +10,30 @@ import java.util.Properties;
 
 public final class DefaultKafkaConsumerTelemetryFactory implements KafkaConsumerTelemetryFactory {
 
+    private static final Tracer NOOP_TRACER = TracerProvider.noop().get("kafka-listener");
+    private static final MeterRegistry NOOP_METER_REGISTRY = new CompositeMeterRegistry();
+
+    @Nullable
     private final Tracer tracer;
+    @Nullable
     private final MeterRegistry meterRegistry;
 
-    public DefaultKafkaConsumerTelemetryFactory(@Nullable Tracer tracer, @Nullable MeterRegistry meterRegistry) {
+    public DefaultKafkaConsumerTelemetryFactory(@Nullable Tracer tracer,
+                                                @Nullable MeterRegistry meterRegistry) {
         this.tracer = tracer;
         this.meterRegistry = meterRegistry;
     }
 
     @Override
     public KafkaConsumerTelemetry get(String listenerName, String listenerImpl, Properties driverProperties, KafkaConsumerTelemetryConfig config) {
-        if (!config.tracing().enabled() && !config.metrics().enabled() && !config.logging().enabled()) {
+        var traceEnabled = this.tracer != null && config.tracing().enabled();
+        var metricEnabled = this.meterRegistry != null && config.metrics().enabled();
+        if (!traceEnabled && !metricEnabled && !config.logging().enabled()) {
             return NoopKafkaConsumerTelemetry.INSTANCE;
         }
 
-        var tracer = config.tracing().enabled() ? this.tracer : TracerProvider.noop().get("kafka-listener");
-        var meterRegistry = config.metrics().enabled() ? this.meterRegistry : new CompositeMeterRegistry();
+        var tracer = traceEnabled ? this.tracer : NOOP_TRACER;
+        var meterRegistry = metricEnabled ? this.meterRegistry : NOOP_METER_REGISTRY;
         return new DefaultKafkaConsumerTelemetry(config, tracer, meterRegistry, listenerName, listenerImpl, driverProperties);
     }
 }
