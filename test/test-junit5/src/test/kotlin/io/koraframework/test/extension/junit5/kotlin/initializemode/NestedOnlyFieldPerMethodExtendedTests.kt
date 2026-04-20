435837cf4c553fd2c45a21d32bb45425a79c0f40
ᴀɴᴛᴏɴ <goodforgod@users.noreply.github.com>
2026-04-21 01:43:42 +0300

    Fixed JUnit test extension graph cleanup in afterAll for `@Nested` test classes `PER_METHOD` (#625)
    * Fixed incorrect JUnit test extension graph cleanup in afterAll for Nested test classes
    * Test reinforced
    
    (cherry picked from commit 89eff4315471d00341f9e8ff70a90efca4ee2c74)

---
+++ test/test-junit5/src/test/kotlin/io/koraframework/test/extension/junit5/kotlin/initializemode/NestedOnlyFieldPerMethodExtendedTests.kt
@@ -0,0 +1,73 @@
+package io.koraframework.test.extension.junit5.kotlin.initializemode
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.Test
+import io.koraframework.test.extension.junit5.TestComponent
+import io.koraframework.test.extension.junit5.testdata.TestComponent12
+
+internal class NestedOnlyFieldPerMethodExtendedTests : AbstractNestedPerMethodTests() {
+
+    @Order(3)
+    @Nested
+    inner class Nested3 {
+
+        @TestComponent
+        lateinit var componentNested12: TestComponent12
+
+        @Test
+        fun test3() {
+            assertNotNull(component1)
+            assertNotNull(component12)
+            if (prevComponent1 == null) {
+                prevComponent1 = component1
+            } else {
+                assertNotSame(prevComponent1, component1)
+            }
+            if (prevComponent12 == null) {
+                prevComponent12 = component12
+            } else {
+                assertNotSame(prevComponent12, component12)
+            }
+        }
+
+        @Test
+        fun test4() {
+            assertNotNull(component1)
+            assertNotNull(component12)
+            assertNotSame(prevComponent1, component1)
+            assertNotSame(prevComponent12, component12)
+            assertNotSame(prevComponent12, componentNested12)
+        }
+    }
+
+    @Order(4)
+    @Nested
+    inner class Nested4 {
+
+        @Test
+        fun test5() {
+            assertNotNull(component1)
+            assertNotNull(component12)
+            if (prevComponent1 == null) {
+                prevComponent1 = component1
+            } else {
+                assertNotSame(prevComponent1, component1)
+            }
+            if (prevComponent12 == null) {
+                prevComponent12 = component12
+            } else {
+                assertNotSame(prevComponent12, component12)
+            }
+        }
+
+        @Test
+        fun test6() {
+            assertNotNull(component1)
+            assertNotNull(component12)
+            assertNotSame(prevComponent1, component1)
+            assertNotSame(prevComponent12, component12)
+        }
+    }
+}
