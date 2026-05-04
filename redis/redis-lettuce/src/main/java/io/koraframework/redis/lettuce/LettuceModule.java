26f856a300d8e5b0e442057714a1a06fbc8a7951
Anton Kurako <goodforgod.dev@gmail.com>
2026-05-04 19:32:01 +0300

    Updated and upgraded Netty dependency and module with IoUring support Added support for NettyModule in LettuceModule to reuse global EventLoop Added and updated module-info.java for netty and lettuce and cache modules

--- redis/redis-lettuce/src/main/java/io/koraframework/redis/lettuce/LettuceModule.java
+++ redis/redis-lettuce/src/main/java/io/koraframework/redis/lettuce/LettuceModule.java
@@ -1,20 +1,21 @@
 package io.koraframework.redis.lettuce;
 
 import io.koraframework.common.DefaultComponent;
+import io.koraframework.common.Tag;
 import io.koraframework.common.util.Configurer;
 import io.koraframework.config.common.Config;
 import io.koraframework.config.common.extractor.ConfigValueExtractor;
+import io.koraframework.netty.common.NettyModule;
 import io.lettuce.core.AbstractRedisClient;
 import io.lettuce.core.ClientOptions;
 import io.lettuce.core.cluster.ClusterClientOptions;
-import io.lettuce.core.cluster.RedisClusterClient;
 import io.lettuce.core.metrics.CommandLatencyRecorder;
 import io.lettuce.core.resource.DefaultClientResources;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.netty.channel.EventLoopGroup;
 import org.jspecify.annotations.Nullable;
 
-public interface LettuceModule {
+public interface LettuceModule extends NettyModule {
 
     default LettuceConfig lettuceConfig(Config config, ConfigValueExtractor<LettuceConfig> extractor) {
         var value = config.get("lettuce");
@@ -24,7 +25,7 @@ public interface LettuceModule {
     @DefaultComponent
     default LettuceFactory lettuceFactory(@Nullable MeterRegistry meterRegistry,
                                           @Nullable CommandLatencyRecorder commandLatencyRecorder,
-                                          @Nullable EventLoopGroup eventLoopGroup,
+                                          @Tag(NettyModule.EventLoopWorker.class) @Nullable EventLoopGroup eventLoopGroup,
                                           @Nullable Configurer<DefaultClientResources.Builder> resourcesConfigurer,
                                           @Nullable Configurer<ClientOptions.Builder> standaloneOptionsConfigurer,
                                           @Nullable Configurer<ClusterClientOptions.Builder> clusterOptionsConfigurer) {
--- redis/redis-lettuce/src/main/java/module-info.java
+++ redis/redis-lettuce/src/main/java/module-info.java
@@ -2,7 +2,14 @@ import org.jspecify.annotations.NullMarked;
 
 @NullMarked
 module kora.redis.lettuce {
+    requires static transitive io.netty.transport.unix.common;
+
+    requires io.netty.transport;
+    requires io.netty.common;
+    requires lettuce.core;
+
     requires kora.telemetry.common;
+    requires kora.netty.common;
     requires kora.common;
 
     exports io.koraframework.redis.lettuce;
