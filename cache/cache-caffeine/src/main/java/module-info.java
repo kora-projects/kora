e1c629d0a0ab63d2b652917ab9a6d6fbc234c6a1
Anton Kurako <goodforgod.dev@gmail.com>
2026-05-05 12:25:26 +0300

    Fix module-info.java and downgrade kotlinpoet

--- cache/cache-caffeine/src/main/java/module-info.java
+++ cache/cache-caffeine/src/main/java/module-info.java
@@ -2,9 +2,12 @@ import org.jspecify.annotations.NullMarked;
 
 @NullMarked
 module kora.cache.caffeine {
-    requires transitive kora.cache.common;
     requires transitive com.github.benmanes.caffeine;
+
+    requires kora.common;
+    requires kora.config.common;
     requires kora.telemetry.common;
+    requires kora.cache.common;
 
     exports io.koraframework.cache.caffeine;
 }
