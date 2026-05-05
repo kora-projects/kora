c186b3fb8cc7d6beff88fe66f93899e4fe157b91
Anton Kurako <goodforgod.dev@gmail.com>
2026-05-05 12:25:26 +0300

    Fix module-info.java and downgrade kotlinpoet

--- cache/cache-redis-lettuce/src/main/java/module-info.java
+++ cache/cache-redis-lettuce/src/main/java/module-info.java
@@ -7,12 +7,13 @@ module kora.cache.redis.lettuce {
     requires transitive io.netty.transport;
     requires transitive io.netty.common;
 
+    requires kora.common;
+    requires kora.config.common;
+    requires kora.telemetry.common;
     requires kora.cache.common;
     requires kora.redis.lettuce;
-    requires lettuce.core;
-    requires kora.common;
-    requires kora.telemetry.common;
     requires kora.json.common;
+    requires lettuce.core;
 
     exports io.koraframework.cache.redis;
     exports io.koraframework.cache.redis.lettuce;
