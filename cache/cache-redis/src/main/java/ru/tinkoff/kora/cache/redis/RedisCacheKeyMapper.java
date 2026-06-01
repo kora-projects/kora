85330a0f67e6bc6b3cbc32948e14225499ca1602
ᴀɴᴛᴏɴ <GoodforGod@users.noreply.github.com>
2026-06-01 12:17:16 +0300

    Added support for null keys and extended mappers for Redis (#641)
    * feat(cache): добавлена поддержка null-ключей и расширенные мапперы для Redis
    - Поддержка null в ключах кэша с выделенными значениями для безопасной сериализации
    - Добавлены мапперы для Boolean, Character, Short, Float, Double, BigDecimal, временных типов и колл
    
    * Reinforced AbstractRedisCache map key & value serialization
    Reinforced and enchanted RedisCacheMapperModule with more mappers

--- cache/cache-redis/src/main/java/ru/tinkoff/kora/cache/redis/RedisCacheKeyMapper.java
+++ cache/cache-redis/src/main/java/ru/tinkoff/kora/cache/redis/RedisCacheKeyMapper.java
@@ -1,5 +1,7 @@
 package ru.tinkoff.kora.cache.redis;
 
+import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
 import ru.tinkoff.kora.cache.CacheKeyMapper;
 
 import java.nio.charset.StandardCharsets;
@@ -14,4 +16,8 @@ public interface RedisCacheKeyMapper<K> extends Function<K, byte[]> {
      * Is used to delimiter composite key such as {@link CacheKeyMapper}
      */
     byte[] DELIMITER = ":".getBytes(StandardCharsets.UTF_8);
+
+    @Nonnull
+    @Override
+    byte[] apply(@Nullable K key);
 }
