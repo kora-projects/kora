47c84e6241d03642efe7a84b4933005eece9cad0
Anton Kurako <goodforgod.dev@gmail.com>
2026-06-09 10:59:44 +0300

    Fixed proper QueryWithParameters trim

--- database/database-annotation-processor/src/main/java/ru/tinkoff/kora/database/annotation/processor/QueryWithParameters.java
+++ database/database-annotation-processor/src/main/java/ru/tinkoff/kora/database/annotation/processor/QueryWithParameters.java
@@ -73,6 +73,8 @@ public record QueryWithParameters(String rawQuery, List<QueryParameter> paramete
             }
         }
 
+        rawSql = rawSql.strip();
+
         var sql = new QueryMacrosParser(types).parse(rawSql, repositoryType, method);
         List<QueryParameter> params = new ArrayList<>();
 
@@ -115,7 +117,7 @@ public record QueryWithParameters(String rawQuery, List<QueryParameter> paramete
             ))
             .toList();
 
-        return new QueryWithParameters(sql.strip(), params);
+        return new QueryWithParameters(sql, params);
     }
 
 
