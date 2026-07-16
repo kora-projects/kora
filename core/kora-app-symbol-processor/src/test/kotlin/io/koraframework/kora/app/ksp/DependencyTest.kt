097e36cd78302275fc47df2510b8a8e3d1aee07f
Anton Duyun <anton.duyun@gmail.com>
2026-07-16 14:01:49 +0300

    fix

--- core/kora-app-symbol-processor/src/test/kotlin/io/koraframework/kora/app/ksp/DependencyTest.kt
+++ core/kora-app-symbol-processor/src/test/kotlin/io/koraframework/kora/app/ksp/DependencyTest.kt
@@ -1,6 +1,7 @@
 package io.koraframework.kora.app.ksp
 
 import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 open class DependencyTest : AbstractKoraAppProcessorTest() {
