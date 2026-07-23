b1cada0db8db010449d3122f773c670c8ef8295e
Anton Kurako <goodforgod.dev@gmail.com>
2026-07-24 00:38:11 +0300

    Added FlywayFactoryModule and LiquibaseFactoryModule

--- database/database-flyway/src/main/java/io/koraframework/database/flyway/FlywayJdbcDatabaseModule.java
+++ database/database-flyway/src/main/java/io/koraframework/database/flyway/FlywayJdbcDatabaseModule.java
@@ -1,15 +1,8 @@
 package io.koraframework.database.flyway;
 
-import io.koraframework.config.common.Config;
-import io.koraframework.config.common.mapper.ConfigValueMapper;
-
 public interface FlywayJdbcDatabaseModule {
 
-    default FlywayConfig flywayConfig(Config config, ConfigValueMapper<FlywayConfig> mapper) {
-        return mapper.mapOrThrow(config.get("flyway"));
-    }
-
-    default FlywayJdbcDatabaseInterceptor flywayJdbcDatabaseInterceptor(FlywayConfig flywayConfig) {
-        return new FlywayJdbcDatabaseInterceptor(flywayConfig);
+    default FlywayFactoryModule flywayFactoryModule() {
+        return new FlywayFactoryModule("flyway");
     }
 }
--- database/database-liquibase/src/main/java/io/koraframework/database/liquibase/LiquibaseJdbcDatabaseModule.java
+++ database/database-liquibase/src/main/java/io/koraframework/database/liquibase/LiquibaseJdbcDatabaseModule.java
@@ -1,15 +1,8 @@
 package io.koraframework.database.liquibase;
 
-import io.koraframework.config.common.Config;
-import io.koraframework.config.common.mapper.ConfigValueMapper;
-
 public interface LiquibaseJdbcDatabaseModule {
 
-    default LiquibaseConfig liquibaseConfig(Config config, ConfigValueMapper<LiquibaseConfig> mapper) {
-        return mapper.mapOrThrow(config.get("liquibase"));
-    }
-
-    default LiquibaseJdbcDatabaseInterceptor liquibaseJdbcDatabaseInterceptor(LiquibaseConfig liquibaseConfig) {
-        return new LiquibaseJdbcDatabaseInterceptor(liquibaseConfig);
+    default LiquibaseFactoryModule liquibaseFactoryModule() {
+        return new LiquibaseFactoryModule("liquibase");
     }
 }
