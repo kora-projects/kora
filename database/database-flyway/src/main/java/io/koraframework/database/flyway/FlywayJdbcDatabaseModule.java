3c64a753f5451cfec40c7506cadd5f42dd05d744
Anton Kurako <goodforgod.dev@gmail.com>
2026-07-31 00:28:27 +0300

    Fixed

--- database/database-flyway/src/main/java/io/koraframework/database/flyway/FlywayJdbcDatabaseModule.java
+++ database/database-flyway/src/main/java/io/koraframework/database/flyway/FlywayJdbcDatabaseModule.java
@@ -1,7 +1,10 @@
 package io.koraframework.database.flyway;
 
+import io.koraframework.common.annotation.FactoryModule;
+
 public interface FlywayJdbcDatabaseModule {
 
+    @FactoryModule
     default FlywayFactoryModule flywayFactoryModule() {
         return new FlywayFactoryModule("flyway");
     }
--- database/database-liquibase/src/main/java/io/koraframework/database/liquibase/LiquibaseJdbcDatabaseModule.java
+++ database/database-liquibase/src/main/java/io/koraframework/database/liquibase/LiquibaseJdbcDatabaseModule.java
@@ -1,7 +1,10 @@
 package io.koraframework.database.liquibase;
 
+import io.koraframework.common.annotation.FactoryModule;
+
 public interface LiquibaseJdbcDatabaseModule {
 
+    @FactoryModule
     default LiquibaseFactoryModule liquibaseFactoryModule() {
         return new LiquibaseFactoryModule("liquibase");
     }
