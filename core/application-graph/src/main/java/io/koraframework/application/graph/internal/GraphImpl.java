b5da449bbb2419ad9d2635fe1a439cde7d19cadd
dsudomoin <155488585+dsudomoin@users.noreply.github.com>
2026-09-21 16:38:36 +0300

    Fixed(application-graph): refresh no longer recreates independent nodes (#880)

--- core/application-graph/src/main/java/io/koraframework/application/graph/internal/GraphImpl.java
+++ core/application-graph/src/main/java/io/koraframework/application/graph/internal/GraphImpl.java
@@ -472,7 +472,7 @@ public final class GraphImpl implements InitializedGraph {
                     init.get();
                 }
             }
-            if (oldObject != null && !node.createDependencies.isEmpty() && node.index != startFrom) {
+            if (oldObject != null && node.index != startFrom) {
                 var dependencyChanged = false;
                 for (var dependency : node.refreshDependencies) {
                     if (rootGraph.get(dependency) != get(dependency)) { // ref equals is intended
